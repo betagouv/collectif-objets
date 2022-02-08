c88e1a17ada6ee6452db674128c8ebd31a4538d1
Adrien Di Pasquale <adrien.di_pasquale@beta.gouv.fr>
2022-02-08 16:00:35 +0100

    added devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :users
   root "pages#home"
 
   resources :objets, only: [:index, :show]
--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_07_103636) do
+ActiveRecord::Schema.define(version: 2022_02_08_145318) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +50,19 @@ ActiveRecord::Schema.define(version: 2022_02_07_103636) do
     t.index ["commune_nom"], name: "index_objets_on_commune_nom"
     t.index ["departement"], name: "index_objets_on_departement"
     t.index ["ref_pop"], name: "objets_unique_ref_pop", unique: true
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at", precision: 6
+    t.datetime "remember_created_at", precision: 6
+    t.datetime "last_sign_in_at", precision: 6
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
