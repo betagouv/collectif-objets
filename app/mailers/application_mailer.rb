8c9b90a8062da1097b4915c941c492ff9c8ad9d7
Adrien Di Pasquale <adrien.di_pasquale@beta.gouv.fr>
2022-02-14 09:16:41 +0100

    setup SIB smtp in prod

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class ApplicationMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: email_address_with_name("collectifobjets@beta.gouv.fr", "Collectif Objets")
   layout "mailer"
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,16 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
-  config.action_mailer.default_url_options = { host: ENV["HOST"], port: 3000 }
+  config.action_mailer.default_url_options = { host: ENV["HOST"] }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => "smtp-relay.sendinblue.com",
+    :port => 587,
+    :user_name => Rails.application.credentials.sendinblue.smtp.username,
+    :password => Rails.application.credentials.sendinblue.smtp.password,
+    :authentication => 'login',
+    :enable_starttls_auto => true
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
