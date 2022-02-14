eda82d5cdade401a4297d499f0cca494bcfe9844
Adrien Di Pasquale <adrien.di_pasquale@beta.gouv.fr>
2022-02-14 18:41:32 +0100

    add current user to sentry context

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,22 @@ class ApplicationController < ActionController::Base
   include Pagy::Backend
 
   before_action :set_locale
+  around_action :set_sentry_context
+
+  protected
+
+  def set_sentry_context
+    yield
+  rescue StandardError => e
+    if current_user
+      Sentry.set_user(
+        id: current_user.id,
+        email: current_user.email,
+        username: current_user.commune&.nom || current_user.email
+      )
+    end
+    raise e
+  end
 
   def set_locale
     I18n.locale = I18n.default_locale
