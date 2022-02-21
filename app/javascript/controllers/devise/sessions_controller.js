3a039f7ddda29c9aa3d4d8ca5cad48bf87f2af77
Adrien Di Pasquale <adrien@dipasquale.fr>
2022-02-21 10:33:13 +0100

    remove debug log

--- app/javascript/controllers/devise/sessions_controller.js
+++ app/javascript/controllers/devise/sessions_controller.js
@@ -2,9 +2,6 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   static targets = ["magicLinkForm"]
-  connect() {
-    console.log("COUCOU!")
-  }
   toggleMagicLinkForm(event) {
     event.currentTarget.setAttribute("disabled", "disabled")
     this.magicLinkFormTarget.classList.remove("hide");
