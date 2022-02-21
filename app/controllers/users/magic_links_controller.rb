05e0881464eb325cd50450ad4a1567ceb90adbab
Adrien Di Pasquale <adrien@dipasquale.fr>
2022-02-21 10:50:25 +0100

    fix lint

--- app/controllers/users/magic_links_controller.rb
+++ app/controllers/users/magic_links_controller.rb
@@ -2,12 +2,9 @@
 
 module Users
   class MagicLinksController < ApplicationController
-    def create
-      email = params.dig(:user, :email)
-      if email.blank?
-        return redirect_to(new_user_session_path, alert: "Veuillez renseigner votre email")
-      end
+    before_action :prevent_missing_email
 
+    def create
       Users::CreateMagicLinkService.new(email).perform => {success:, error:}
 
       if success
@@ -16,6 +13,18 @@ module Users
         flash.alert = "Aucun compte n'a été trouvé pour l'email #{email}"
       end
       redirect_to(new_user_session_path(user: { email: }, success:, error:))
+    end
+
+    protected
+
+    def email
+      params.dig(:user, :email)
+    end
+
+    def prevent_missing_email
+      return true if email.present?
+
+      redirect_to(new_user_session_path, alert: "Veuillez renseigner votre email")
     end
   end
 end
