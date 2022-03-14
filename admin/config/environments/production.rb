72ac819e1209c9114322542556fdc2c27be6191b
Adrien Di Pasquale <adrien.di_pasquale@beta.gouv.fr>
2022-03-14 10:23:27 +0100

    [admin] fix active storage attachment urls

--- admin/config/environments/production.rb
+++ admin/config/environments/production.rb
@@ -1,5 +1,7 @@
 require "active_support/core_ext/integer/time"
 
+Rails.application.default_url_options = { host: ENV["HOST"] }
+
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
