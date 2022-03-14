d339f9a31744c7275605c0beb67e747db25b8f03
Adrien Di Pasquale <adrien.di_pasquale@beta.gouv.fr>
2022-03-14 15:02:15 +0100

    [admin] improve devise email + www new job sib move contact lists

--- www/app/lib/co/send_in_blue_client.rb
+++ www/app/lib/co/send_in_blue_client.rb
@@ -47,15 +47,19 @@ module Co
 
     def initialize; end
 
-    def get_list_contacts(departement, name, acc = [])
+    def get_list_contacts(departement, name)
+      get_list_id_contacts(get_list_id!(departement, name))
+    end
+
+    def get_list_id_contacts(list_id, acc = [])
       res = contacts_client.get_contacts_from_list(
-        get_list_id!(departement, name),
+        list_id,
         limit: PER_PAGE, # does not seem to be respected
         offset: acc.count
       )
       sleep(1)
       acc += res.contacts
-      res.count == PER_PAGE ? get_list_contacts(departement, name, acc) : acc
+      res.count == PER_PAGE ? get_list_id_contacts(list_id, acc) : acc
     end
 
     def get_list(departement, name)
@@ -67,19 +71,25 @@ module Co
     end
 
     def remove_contact_from_list(email, departement, list_name)
+      remove_contact_from_list_id(email, get_list_id!(departement, list_name))
+    end
+
+    def add_contact_to_list(email, departement, list_name)
+      add_contact_to_list_id(email, get_list_id!(departement, list_name))
+    end
+
+    def remove_contact_from_list_id(email, list_id)
       contact = contacts_client.get_contact_info(email)
-      list_id = get_list_id!(departement, list_name)
       return if contact.list_ids.exclude?(list_id)
 
       contacts_client.remove_contact_from_list(list_id, { emails: [email] })
     end
 
-    def add_contact_to_list(email, departement, list_name)
+    def add_contact_to_list_id(email, list_id)
       contact = contacts_client.get_contact_info(email)
-      list_id = get_list_id!(departement, list_name)
       return if contact.list_ids.include?(list_id)
 
-      contacts_client.add_contact_to_list(get_list_id!(departement, list_name), { emails: [email] })
+      contacts_client.add_contact_to_list(list_id, { emails: [email] })
     end
 
     private
