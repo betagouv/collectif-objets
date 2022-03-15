a08e0d6d2c0d10b1c18102552bcee4b321e2717e
Adrien Di Pasquale <adrien.di_pasquale@beta.gouv.fr>
2022-03-15 11:20:16 +0100

    replace letter opener with mailcatcher in dev

--- admin/config/environments/development.rb
+++ admin/config/environments/development.rb
@@ -39,9 +39,10 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.delivery_method = :letter_opener
-  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { :address => '127.0.0.1', :port => 1025 }
   config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.perform_caching = false
 
   config.action_mailer.perform_caching = false
 
--- www/config/environments/development.rb
+++ www/config/environments/development.rb
@@ -40,10 +40,9 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
-  config.action_mailer.delivery_method = :letter_opener
-  config.action_mailer.perform_deliveries = true
-  # Don't care if the mailer can't send.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { :address => '127.0.0.1', :port => 1025 }
+  config.action_mailer.raise_delivery_errors = false
   config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
