d9829d33e715fdeb2d6eb056b37314cd13abf997
Adrien Di Pasquale <adrien.di_pasquale@beta.gouv.fr>
2022-03-17 11:22:32 +0100

    in enrollment form add deadline by departements - fix #41

---
+++ www/app/lib/co/campaigns.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+module Co
+  module Campaigns
+    DEADLINES = {
+      "51" => Date.new(2022, 4, 15),
+      "65" => Date.new(2022, 5, 15)
+    }.freeze
+  end
+end
