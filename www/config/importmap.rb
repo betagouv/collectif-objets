3c6aa535e7270c3dbad1b6522851509baca2abc7
Adrien Di Pasquale <adrien@dipasquale.fr>
2022-04-14 15:18:12 +0200

    fixing sentry imports

--- www/config/importmap.rb
+++ www/config/importmap.rb
@@ -8,11 +8,3 @@ pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin "@rails/activestorage", to: "activestorage.esm.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
-pin "@sentry/browser", to: "@sentry--browser.js" # @6.19.6
-pin "@sentry/tracing", to: "@sentry--tracing.js" # @6.19.6
-pin "@sentry/core", to: "@sentry--core.js" # @6.19.6
-pin "@sentry/hub", to: "@sentry--hub.js" # @6.19.6
-pin "@sentry/minimal", to: "@sentry--minimal.js" # @6.19.6
-pin "@sentry/types", to: "@sentry--types.js" # @6.19.6
-pin "@sentry/utils", to: "@sentry--utils.js" # @6.19.6
-pin "tslib" # @1.14.1
--- www/vendor/javascript/@sentry--types.js
+++
@@ -1,2 +0,0 @@
-var r;(function(r){r.Fatal="fatal";r.Error="error";r.Warning="warning";r.Log="log";r.Info="info";r.Debug="debug";r.Critical="critical"})(r||(r={}));var a=["fatal","error","warning","log","info","debug","critical"];export{r as Severity,a as SeverityLevels};
-
