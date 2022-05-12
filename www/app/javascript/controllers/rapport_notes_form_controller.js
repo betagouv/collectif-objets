8f4f9a8e41283e4ad15dedd5781ed847db8c0be8
Adrien Di Pasquale <adrien.di_pasquale@beta.gouv.fr>
2022-05-12 13:52:47 +0200

    minor fixes

--- www/app/javascript/controllers/rapport_notes_form_controller.js
+++ www/app/javascript/controllers/rapport_notes_form_controller.js
@@ -3,12 +3,7 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["textarea", "submit"]
 
-  connect() {
-    console.log("waza 1")
-  }
-
   toggleDisabled() {
-    console.log("waza")
     this.submitTarget.toggleAttribute(
       "disabled",
       this.textareaTarget.value == this.textareaTarget.dataset.originalValue
