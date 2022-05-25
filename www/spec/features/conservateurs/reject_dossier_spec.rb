e88075a9132d0694a5663142fbd27dfc711fd09e
Adrien Di Pasquale <adrien@dipasquale.fr>
2022-05-25 18:37:58 +0200

    fixing flaky spec - fix #110

--- www/spec/features/conservateurs/reject_dossier_spec.rb
+++ www/spec/features/conservateurs/reject_dossier_spec.rb
@@ -51,6 +51,7 @@ RSpec.feature "Conservateurs - Reject Dossier", type: :feature, js: true do
       .find(:xpath, "ancestor::div[contains(@class, 'attribute-group')]")
     within(etat_sanitaire_group) do
       click_on "Modifier"
+      sleep 1
       select "En péril", from: "recensement[analyse_etat_sanitaire]"
     end
     click_on "Sauvegarder"
