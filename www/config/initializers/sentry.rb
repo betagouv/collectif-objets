0fb8e4bacbdd6d038e12cc0e5e2fca016fc9fc47
Adrien Di Pasquale <adrien.di_pasquale@beta.gouv.fr>
2022-05-30 10:55:22 +0200

    point sentry to incubateur server - fix #65

--- www/config/initializers/sentry.rb
+++ www/config/initializers/sentry.rb
@@ -1,7 +1,7 @@
 Sentry.init do |config|
   return unless Rails.env.production?
 
-  config.dsn = 'https://29778c73190d4c4db4c3369d6ed15df7@o103100.ingest.sentry.io/6204702'
+  config.dsn = 'https://5f6f9cf638ac413b82d1d9c8a9ba2025@sentry.incubateur.net/40'
   config.breadcrumbs_logger = [:active_support_logger, :http_logger]
 
   # Set tracesSampleRate to 1.0 to capture 100%
