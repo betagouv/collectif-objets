2c979d5b75d4da0b12ff3d29d2a43fdb163f85ce
Adrien Di Pasquale <adrien@dipasquale.fr>
2022-07-26 11:22:35 +0200

    fix gallery problem

--- app/components/gallery_component.rb
+++ app/components/gallery_component.rb
@@ -9,7 +9,7 @@ class GalleryComponent < ViewComponent::Base
   delegate :count, to: :photos_structs
 
   def self.from_attachments(attachments, **kwargs)
-    new(attachments.map { PHOTO_STRUCT.new(_1.url, _1.variant(:medium).url) }, **kwargs)
+    new(attachments.map { PHOTO_STRUCT.new(_1, _1.variant(:medium)) }, **kwargs)
   end
 
   def self.from_urls(original_urls, **kwargs)
