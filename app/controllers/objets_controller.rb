8ae51c61dc5a4d136a97f355146a29ee98c0cc8b
Adrien Di Pasquale <adrien@dipasquale.fr>
2022-08-08 09:09:12 +0200

    reparer bug objets sans commune

--- app/controllers/objets_controller.rb
+++ app/controllers/objets_controller.rb
@@ -5,11 +5,7 @@ class ObjetsController < ApplicationController
     @filters = {
       commune: params[:commune_code_insee].present? ? Commune.find_by(code_insee: params[:commune_code_insee]) : nil
     }.compact
-    @pagy, @objets = pagy(
-      Objet
-        .where(@filters[:commune] ? { commune_code_insee: @filters[:commune].code_insee } : nil)
-        .with_photos_first
-    )
+    @pagy, @objets = pagy(filtered_objets.with_photos_first)
   end
 
   def show
@@ -19,5 +15,14 @@ class ObjetsController < ApplicationController
   def show_by_ref_pop
     @objet = Objet.find_by(palissy_REF: params[:palissy_REF])
     render :show
+  end
+
+  private
+
+  def filtered_objets
+    o = Objet.all
+    o = o.where(commune_code_insee: @filters[:commune].code_insee) if @filters[:commune]
+    o = o.where_assoc_exists(:commune) if @filters[:commune].blank?
+    o
   end
 end
