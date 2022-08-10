3365dae55e249341b16e076d121686f42b326fbb
Adrien Di Pasquale <adrien.di_pasquale@beta.gouv.fr>
2022-08-10 17:39:56 +0200

    script to seed review apps (#220)
    * script to seed review apps
    
    * set lookbook gem as dev only dep
    
    * fixing script
    
    * fix routes lookbook

---
+++ scripts/restore_seed_db.sh
@@ -0,0 +1,4 @@
+rails db:schema:load
+
+curl https://s3.fr-par.scw.cloud/collectif-objets-public/seeds.pgsql | \
+  pg_restore --data-only --clean --if-exists --no-owner --no-privileges --no-comments --dbname $SCALINGO_POSTGRESQL_URL
