e7ebf99789e6b2eca5f8b72e64d4bcc69d8c1a50
Adrien Di Pasquale <adrien@dipasquale.fr>
2022-09-05 17:47:23 +0200

    WIP vite imagetools

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
 import RubyPlugin from 'vite-plugin-ruby'
 import StimulusHMR from 'vite-plugin-stimulus-hmr'
+import { imagetools } from 'vite-imagetools'
 
 export default defineConfig({
   plugins: [
     RubyPlugin(),
-    StimulusHMR()
+    StimulusHMR(),
+    imagetools()
   ],
 })
