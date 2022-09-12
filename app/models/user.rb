4a4ad02790649c34ed977159fb4425b7b18e1662
Adrien Di Pasquale <adrien@dipasquale.fr>
2022-09-12 15:33:09 +0200

    add mailcatch to safe domains

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@
 class User < ApplicationRecord
   ROLE_MAIRIE = "mairie"
   ROLES = [ROLE_MAIRIE].freeze
-  SAFE_DOMAINS = ["beta.gouv.fr", "dipasquale.fr", "failfail.fr"].freeze
+  SAFE_DOMAINS = ["beta.gouv.fr", "dipasquale.fr", "failfail.fr", "mailcatch.com"].freeze
 
   devise :database_authenticatable, :recoverable, :rememberable, :validatable, :registerable
 
