56609085373e0f280ff6d56ec780eabf233e6549
Adrien Di Pasquale <adrien@dipasquale.fr>
2022-09-13 16:56:33 +0200

    fixing script create_sentry_release

--- scripts/create_sentry_release.sh
+++ scripts/create_sentry_release.sh
@@ -1,6 +1,27 @@
 [[ $HOST == *"staging"* ]] && SENTRY_ENV="staging" || SENTRY_ENV="production"
-sentry-cli releases new "$CONTAINER_VERSION"
-sentry-cli releases files "$CONTAINER_VERSION" upload-sourcemaps /app/public/vite/assets
-sentry-cli releases finalize "$CONTAINER_VERSION"
-sentry-cli releases deploys "$CONTAINER_VERSION" new -env "$SENTRY_ENV" --project collectif-objets
-sentry-cli releases deploys "$CONTAINER_VERSION" new -env "$SENTRY_ENV" --project collectif-objets-js
+
+# build for rails
+NAME="$CONTAINER_VERSION-rails"
+if sentry-cli releases info --project collectif-objets $NAME &>1
+then
+  echo "using existing sentry release $NAME"
+  sentry-cli releases deploys $NAME new --env "$SENTRY_ENV" --project collectif-objets
+else
+  echo "building new sentry release $NAME"
+  sentry-cli releases new --project collectif-objets --finalize $NAME
+  sentry-cli releases deploys $NAME new --env "$SENTRY_ENV" --project collectif-objets
+fi
+
+# build for JS
+NAME="$CONTAINER_VERSION-js"
+if sentry-cli releases info --project collectif-objets-js $NAME &>1
+then
+  echo "using existing sentry release $NAME"
+  sentry-cli releases deploys $NAME new --env "$SENTRY_ENV" --project collectif-objets-js
+else
+  echo "building new sentry release $NAME"
+  sentry-cli releases new --project collectif-objets-js $NAME
+  sentry-cli releases files $NAME upload-sourcemaps --project collectif-objets-js /app/public/vite/assets
+  sentry-cli releases finalize --project collectif-objets-js $NAME
+  sentry-cli releases deploys $NAME new --env "$SENTRY_ENV" --project collectif-objets-js
+fi
