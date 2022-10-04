63e70420cfab0af2214e5908a76dceef318b1456
Adrien Di Pasquale <adrien@dipasquale.fr>
2022-10-04 10:06:29 +0200

    hotfix reset password instructions for admin users

--- app/mailers/custom_devise_mailer.rb
+++ app/mailers/custom_devise_mailer.rb
@@ -7,7 +7,7 @@ class CustomDeviseMailer < Devise::Mailer
 
   def reset_password_instructions(record, token, opts = {})
     @token = token
-    if record.last_sign_in_at?
+    if record.respond_to?(:last_sign_in_at) && record.last_sign_in_at?
       devise_mail(record, :reset_password_instructions, opts)
     else
       devise_mail(record, :define_password_instructions, opts)
