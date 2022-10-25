2aa03569169bb6bffa8773a2892256fa59510bbc
Adrien Di Pasquale <adrien.di_pasquale@beta.gouv.fr>
2022-10-25 11:25:04 +0200

    ajout d'info pour les departements concordataires dans les emails de campagnes (#374)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_06_062815) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_25_091308) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -169,6 +169,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_06_062815) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "dans_nom"
+    t.boolean "concordataire"
   end
 
   create_table "dossiers", force: :cascade do |t|
