39e632cf4082bcee699cea86969fdba831c90107
Adrien Di Pasquale <adrien.di_pasquale@beta.gouv.fr>
2022-10-27 10:43:59 +0200

    refonte des pages d'aides et des textes sur la vue commune (#285)
    fix #215
    fix #284

---
+++ app/components/callout_with_cta_component.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+class CalloutWithCtaComponent < ViewComponent::Base
+  renders_one :text
+  renders_one :cta
+
+  def initialize(color: nil)
+    @color = color
+    super
+  end
+
+  def callout_color_class
+    return nil if @color.nil?
+
+    "fr-callout--#{@color}"
+  end
+end
