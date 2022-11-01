386e29522ebadf29391d82140699a1dee280e4cf
Adrien Di Pasquale <adrien@dipasquale.fr>
2022-11-01 22:17:48 +0100

    remove mailer helper

--- app/helpers/mailer_helper.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module MailerHelper
-  def png_base64(filename)
-    Rails.root.join("app/frontend/images/", filename).open("rb") do |img|
-      "data:image/png;base64,#{Base64.strict_encode64(img.read)}"
-    end
-  end
-end
