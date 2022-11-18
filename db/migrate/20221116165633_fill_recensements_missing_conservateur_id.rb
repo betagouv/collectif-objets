9e4676dc89e74e4f195e544eb3a2b3d55b6e3f70
Adrien Di Pasquale <adrien@dipasquale.fr>
2022-11-18 14:21:54 +0100

    reparation du bug de stockage du conservateur sur les recensements

--- db/migrate/20221116165633_fill_recensements_missing_conservateur_id.rb
+++ db/migrate/20221116165633_fill_recensements_missing_conservateur_id.rb
@@ -9,19 +9,19 @@ class FillRecensementsMissingConservateurId < ActiveRecord::Migration[7.0]
       end
 
     [
-      # { code_insee: "21040", nom: "Avosnes", conservateur: "" },
-      # { code_insee: "21100", nom: "Brain", conservateur: "" },
-      # { code_insee: "21173", nom: "Chorey-les-Beaune", conservateur: "" },
-      # { code_insee: "21197", nom: "Corpoyer-la-Chapelle", conservateur: "" },
-      # { code_insee: "21304", nom: "Grancey-le-Château-Neuvelle", conservateur: "" },
-      # { code_insee: "21596", nom: "Savouges", conservateur: "" },
+      { code_insee: "21040", nom: "Avosnes", conservateur: "adeline-e.riviere" },
+      { code_insee: "21100", nom: "Brain", conservateur: "adeline-e.riviere" },
+      { code_insee: "21173", nom: "Chorey-les-Beaune", conservateur: "adeline-e.riviere" },
+      { code_insee: "21197", nom: "Corpoyer-la-Chapelle", conservateur: "adeline-e.riviere" },
+      { code_insee: "21304", nom: "Grancey-le-Château-Neuvelle", conservateur: "chb.hd.b.francois" },
+      { code_insee: "21596", nom: "Savouges", conservateur: "adeline-e.riviere" },
       { code_insee: "51141", nom: "La Chaussée-sur-Marne", conservateur: "frederic.murienne" },
       { code_insee: "51204", nom: "Damery", conservateur: "frederic.murienne" },
       { code_insee: "51485", nom: "Saint-Hilaire-au-Temple", conservateur: "frederic.murienne" },
       { code_insee: "51513", nom: "Saint-Rémy-en-Bouzemont-Saint-Genest-et-Isson", conservateur: "frederic.murienne" },
       { code_insee: "59159", nom: "Craywick", conservateur: "marie.boudeele" },
       { code_insee: "59436", nom: "Noordpeene", conservateur: "marie.boudeele" },
-      Noordpeene (
+      { code_insee: "59436", nom: "Warhem", conservateur: "marie.boudeele" },
     ].each do |row|
       recensements = recensements_with_missing_conservateur
         .joins(dossier: [:commune])
