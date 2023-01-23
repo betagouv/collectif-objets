214aab7e3d463f555a59ac7f83d2a6d8e44a1514
Adrien Di Pasquale <adrien@dipasquale.fr>
2023-01-23 17:21:47 +0100

    use support email in campaign mails

--- lib/mailer_previews/application_mailer_preview.rb
+++ lib/mailer_previews/application_mailer_preview.rb
@@ -6,4 +6,10 @@ class ApplicationMailerPreview < ActionMailer::Preview
   def build(*args, **kwargs)
     FactoryBot.build(*args, **kwargs).tap(&:readonly!)
   end
+
+  def build_list(*args, **kwargs)
+    list = FactoryBot.build_list(*args, **kwargs)
+    list.each(&:readonly!)
+    list
+  end
 end
--- lib/mailer_previews/campaign_v1_mailer_preview.rb
+++ lib/mailer_previews/campaign_v1_mailer_preview.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-# rubocop:disable Style/OpenStructUse
-
 class CampaignV1MailerPreview < ApplicationMailerPreview
   CampaignV1Mailer::MAIL_NAMES.each do |name|
     define_method "#{name}_email" do
@@ -13,36 +11,19 @@ class CampaignV1MailerPreview < ApplicationMailerPreview
   protected
 
   def user
-    @user ||= User.new(email: "mairie@thoiry.fr", login_token: "fakecode").tap(&:readonly!)
+    @user ||= build :user, email: "mairie@thoiry.fr", login_token: "fakecode"
   end
 
   def commune
-    commune ||= Commune.new(
-      nom: "Thoiry", code_insee: "23300", departement_code: "23", status: @commune_status
-    ).tap(&:readonly!)
-
-    def commune.objets
-      OpenStruct.new(count: 20)
-    end
-
-    def commune.highlighted_objet
-      OpenStruct.new(
-        palissy_photos: [],
-        palissy_TICO: "Grande table Louis XVI",
-        edifice_nom_formatted: "Église St-Baptiste",
-        emplacement: "Sacristie"
-      )
-    end
+    commune ||= build \
+      :commune,
+      nom: "Thoiry", code_insee: "23300", departement_code: "23", status: @commune_status,
+      objets: build_list(:objet, 10)
+    def commune.highlighted_objet = objets.first
     @commune ||= commune
   end
 
   def campaign
-    campaign = FactoryBot.build(:campaign)
-    def campaign.communes
-      OpenStruct.new(count: 304)
-    end
-    @campaign ||= campaign
+    @campaign ||= build :campaign, communes: build_list(:commune, 10)
   end
 end
-
-# rubocop:enable Style/OpenStructUse
