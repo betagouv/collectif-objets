73addb2bcbadc26451af1d2593d2e74ee7f60f99
Adrien Di Pasquale <adrien.di_pasquale@beta.gouv.fr>
2023-02-15 19:39:23 +0100

    amélioration de la prévisualisation des emails d’acceptation de dossier (#702)

--- app/frontend/stimulus_controllers/rapport_notes_form_controller.js
+++ app/frontend/stimulus_controllers/rapport_notes_form_controller.js
@@ -1,17 +1,22 @@
 import { Controller } from "@hotwired/stimulus"
-import { debounce } from 'throttle-debounce';
 
 export default class extends Controller {
-  static targets = ["container", "form", "textarea", "submit", "accordionButton", "previewExpandedInput", "previewOverlay"]
 
-  connect() {
-    this.debouncedSubmit = debounce(1000, () => this.formTarget.requestSubmit()).bind(this)
-    this.textareaTarget.addEventListener("input", this.debouncedSubmit)
-    this.textareaTarget.addEventListener("input", () => this.containerTarget.classList.add("loading"))
+  static targets = ["previewPanel", "notesTextarea", "accordionButton"]
+
+  closePreview() {
+    if (this.accordionButtonTarget.getAttribute("aria-expanded") == "false") return
+
+    this.accordionButtonTarget.setAttribute("aria-expanded", "false")
+    setTimeout(() => this.notesTextareaTarget.focus(), 100)
   }
 
-  toggleAccordion() {
-    const newValue = this.accordionButtonTarget.getAttribute("aria-expanded") == "false"
-    this.previewExpandedInputTarget.value = newValue
+  submitPreview(event) {
+    if (event.currentTarget.getAttribute("aria-expanded") != "false") return
+
+    const form = this.previewPanelTarget.querySelector("form")
+    const hiddenField = this.previewPanelTarget.querySelector('input[name="dossier[notes_conservateur]"]')
+    hiddenField.value = this.notesTextareaTarget.value
+    form.requestSubmit()
   }
 }
