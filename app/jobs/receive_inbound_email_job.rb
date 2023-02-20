204fbb5f3a54e4add504d1c5398ca04ceb3d5294
Adrien Di Pasquale <adrien@dipasquale.fr>
2023-02-20 14:22:26 +0100

    réparation des inbound emails reçus en CC

--- app/jobs/receive_inbound_email_job.rb
+++ app/jobs/receive_inbound_email_job.rb
@@ -4,9 +4,24 @@ class ReceiveInboundEmailJob
   include Sidekiq::Job
 
   def perform(raw)
-    message = InboundEmail.from_raw(raw).to_message
+    @raw = raw
+    validate_inbound_email!
+
+    message = inbound_email.to_message
     message.save!
     message.enqueue_message_received_emails
     message.inbound_email.enqueue_download_attachments
+  end
+
+  private
+
+  def validate_inbound_email!
+    return true if inbound_email.valid?
+
+    raise ArgumentError, "invalid InboundEmail : #{inbound_email.errors.full_messages.to_sentence}"
+  end
+
+  def inbound_email
+    @inbound_email ||= InboundEmail.from_raw(@raw)
   end
 end
