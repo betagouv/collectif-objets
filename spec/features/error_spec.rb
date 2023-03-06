8e4ce7a0a55fe1d10a18841bf4f17aee28cecb67
Adrien Di Pasquale <adrien@dipasquale.fr>
2023-03-06 09:07:11 +0100

    simplify

--- spec/features/error_spec.rb
+++ spec/features/error_spec.rb
@@ -3,12 +3,6 @@
 require "rails_helper"
 
 RSpec.feature "Error page", type: :feature, js: true do
-  around :each do |example|
-    Rails.application.config.consider_all_requests_local = false
-    example.run
-    Rails.application.config.consider_all_requests_local = true
-  end
-
   scenario "visit health page" do
     visit "/"
     visit "/health/raise_on_purpose"
