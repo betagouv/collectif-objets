6a73bda93188b4817855db216b9fa9ae6dbe942e
Adrien Di Pasquale <adrien@dipasquale.fr>
2023-03-07 08:18:11 +0100

    improve migrations

--- db/migrate/20230220083444_add_recensements_status.rb
+++ db/migrate/20230220083444_add_recensements_status.rb
@@ -1,4 +1,4 @@
-class AddDraftToRecensements < ActiveRecord::Migration[7.0]
+class AddRecensementsStatus < ActiveRecord::Migration[7.0]
   def up
     add_column :recensements, :status, :string, default: "draft", null: false
     Recensement.update_all(status: "completed")
