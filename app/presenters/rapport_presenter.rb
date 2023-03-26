fc940abbef46a9ed8fc6638695f26b66ad5fd247
Adrien Di Pasquale <adrien@dipasquale.fr>
2023-03-26 20:14:12 +0200

    wip

--- app/presenters/rapport_presenter.rb
+++ app/presenters/rapport_presenter.rb
@@ -1,28 +1,27 @@
 # frozen_string_literal: true
 
 class RapportPresenter
-  RecensementItem = Struct.new(:recensement, :objet, :presenter, :index)
-  FicheItem = Struct.new(:fiche, :recensement_items, :index)
-
   delegate :commune, :accepted_at, :conservateur, :notes_commune, :notes_conservateur, to: :dossier
 
   def initialize(dossier)
     @dossier = dossier
   end
 
-  def recensement_items
-    ordered_recensements.each_with_index.map do |recensement, index|
-      RecensementItem.new(recensement, recensement.objet, RecensementPresenter.new(recensement), index + 1)
+  def each_recensement_item
+    ordered_recensements.each_with_index do |recensement, index|
+      yield recensement:,
+            objet: recensement.objet,
+            commune: recensement.commune,
+            presenter: RecensementPresenter.new(recensement),
+            index: index + 1
     end
   end
 
-  def fiche_items
-    ordered_recensements.map(&:analyse_fiches).flatten.uniq.sort.each_with_index.map do |fiche_id, index|
-      FicheItem.new(
-        Fiche.load_from_id(fiche_id),
-        ordered_recensements.select { _1.analyse_fiches.include?(fiche_id) },
-        index + 1
-      )
+  def each_fiche_item
+    ordered_recensements.map(&:analyse_fiches).flatten.uniq.sort.each_with_index do |fiche_id, index|
+      yield fiche: Fiche.load_from_id(fiche_id),
+            recensements: ordered_recensements.select { _1.analyse_fiches.include?(fiche_id) },
+            index: index + 1
     end
   end
 
