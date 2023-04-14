9b7da319684989529234458227f10c91175d2b3a
Adrien Di Pasquale <adrien@dipasquale.fr>
2023-04-14 08:34:54 +0200

    fix specs

--- spec/features/communes/sign_in_with_token_spec.rb
+++ spec/features/communes/sign_in_with_token_spec.rb
@@ -7,14 +7,17 @@ RSpec.feature "Sign in with token", type: :feature, js: true do
   let!(:commune) { create(:commune, nom: "Albon", code_insee: "26002", departement:) }
   let!(:user) { create(:user, email: "mairie-albon@test.fr", role: "mairie", commune:, magic_token: "magiemagie") }
 
+  include ActiveJob::TestHelper
+
   scenario "sign in with token" do
     visit "/"
     click_on "Connexion commune"
     expect(page).to have_text("Connexion")
     fill_in "Email", with: "mairie-albon@test.fr"
     click_on "Recevoir un lien de connexion"
-
     expect(page).to have_text("Veuillez cliquer sur le lien que vous avez reçu par mail pour vous connecter")
+
+    perform_enqueued_jobs
     email = ActionMailer::Base.deliveries.last
     res = email.html_part.decoded.match(%r{/users/sign_in_with_token\?login_token=([a-z0-9]+)})
     expect(res).not_to be_nil
