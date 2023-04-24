ba3e8003ecd70e4bd039aac50e927976cc528bec
Adrien Di Pasquale <adrien.di_pasquale@beta.gouv.fr>
2023-04-24 08:56:23 +0200

    simplification du JS pour imprimer les rapports (#818)

--- app/frontend/stimulus_controllers/print_controller.js
+++ app/frontend/stimulus_controllers/print_controller.js
@@ -1,23 +1,8 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-
-  static targets = ["accordionMember"]
-
   print(event) {
     event.preventDefault()
-    if (this.hasAccordionMemberTarget) {
-      this.accordionMemberTargets.forEach(t => this.discloseAccordionMember(t))
-    }
-    window.setTimeout(() => window.print(), 300)
-  }
-
-  discloseAccordionMember(accordionMember) {
-    if (!window.dsfr) return
-    const accordionGroup = accordionMember.closest("[data-fr-js-accordions-group]")
-    if (!accordionGroup) return
-    const index = [...accordionGroup.children].indexOf(accordionMember);
-    if (index < 0) return
-    window.dsfr(accordionGroup).accordionsGroup.members[index].disclose()
+    window.print()
   }
 }
