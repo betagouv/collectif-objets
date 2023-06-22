5e8f0e68dba804e54a25ecb71d02b832f47f23d1
Brice Durand <brice.durand2@gmail.com>
2023-06-22 16:10:57 +0200

    Plus de clareté sur le  test sur le caractère inscrit ou classé d'un objet

--- spec/models/objet_spec.rb
+++ spec/models/objet_spec.rb
@@ -4,76 +4,88 @@ require "rails_helper"
 
 RSpec.describe Objet, type: :model do
   it "filtre les objets inscrits et classés au patrimoine" do
-    create(:objet, palissy_PROT: "1993/01/11 : inscrit au titre objet") # inscrit
-    create(:objet, palissy_PROT: "classé au ti") # classé
-    create(:objet, palissy_PROT: "classé au titre") # classé
-    create(:objet, palissy_PROT: "classé au titre ensemble historique mobilier") # classé
-    create(:objet, palissy_PROT: "classé au titre immeuble") # classé
-    create(:objet, palissy_PROT: "classé au titre immeuble ; classé au titre objet") # classé
-    create(:objet, palissy_PROT: "classé au titre immeuble ; classé au titre objet ; classé au titre objet") # classé
-    create(:objet, palissy_PROT: "classé au titre immeuble ; inscrit au titre objet") # mis de côté
-    create(:objet, palissy_PROT: "classé  au titre objet") # classé
-    create(:objet, palissy_PROT: "classé au  titre objet") # classé
-    create(:objet, palissy_PROT: "classé au titre objet") # classé
-    create(:objet, palissy_PROT: "classé au titre objet ") # classé
-    create(:objet, palissy_PROT: "classé au titre objet (?)") # classé
-    create(:objet, palissy_PROT: "classé au titre objet.") # classé
-    create(:objet, palissy_PROT: "Classé au titre objet") # classé
-    create(:objet, palissy_PROT: "classé au titre objet ; classé au titre immeuble") # classé
-    create(:objet, palissy_PROT: "classé au titre objet ; classé au titre objet") # classé
-    create(:objet, palissy_PROT: "classé au titre objet ; classé au titre objet ; classé au titre objet") # classé
-    create(:objet, palissy_PROT: "classé au titre objet ; classé au titre objet ; inscrit au titre objet") # mis de côté
-    create(:objet, palissy_PROT: "classé au titre objet ; inscrit au titre immeuble") # mis de côté
-    create(:objet, palissy_PROT: "classé au titre objet ; inscrit au titre objet") # mis de côté
-    create(:objet, palissy_PROT: "classé au titre objet ; inscrit au titre objet partiellement") # mis de côté
-    create(:objet, palissy_PROT: "classé au titre objet partiellement") # classé
-    create(:objet, palissy_PROT: "classé comme ensemble historique mobilier") # classé
-    create(:objet, palissy_PROT: "classé comme un ensemble historique mobilier") # classé
-    create(:objet, palissy_PROT: "classé MH") # classé
-    create(:objet, palissy_PROT: "classé MH ; classé au titre objet") # classé
-    create(:objet, palissy_PROT: "classé Monument Historique") # classé
-    create(:objet, palissy_PROT: "déclassé au titre objet") # mis de côté
-    create(:objet, palissy_PROT: "désinscrit") # mis de côté
-    create(:objet, palissy_PROT: "devenu non protégé") # mis de côté
-    create(:objet, palissy_PROT: "fleur") # mis de côté
-    create(:objet, palissy_PROT: "i") # mis de côté
-    create(:objet, palissy_PROT: "immeuble par nature") # mis de côté
-    create(:objet, palissy_PROT: "inscr") # inscrit
-    create(:objet, palissy_PROT: "inscri") # inscrit
-    create(:objet, palissy_PROT: "inscrit") # inscrit
-    create(:objet, palissy_PROT: "inscrit au") # inscrit
-    create(:objet, palissy_PROT: "inscrit au ") # inscrit
-    create(:objet, palissy_PROT: "inscrit au t") # inscrit
-    create(:objet, palissy_PROT: "inscrit au tit") # inscrit
-    create(:objet, palissy_PROT: "inscrit au titr") # inscrit
-    create(:objet, palissy_PROT: "inscrit au titre") # inscrit
-    create(:objet, palissy_PROT: "inscrit au titre des Monuments historiques") # inscrit
-    create(:objet, palissy_PROT: "inscrit au titre immeuble") # inscrit
-    create(:objet, palissy_PROT: "inscrit au titre obejt") # inscrit
-    create(:objet, palissy_PROT: "inscrit au titre obje") # inscrit
-    create(:objet, palissy_PROT: " inscrit au titre objet") # inscrit
-    create(:objet, palissy_PROT: "\ninscrit au titre objet") # inscrit
-    create(:objet, palissy_PROT: "inscrit au titre objet") # inscrit
-    create(:objet, palissy_PROT: "inscrit au titre objet (?)") # inscrit
-    create(:objet, palissy_PROT: "Inscrit au titre objet") # inscrit
-    create(:objet, palissy_PROT: "inscrit au titre objet ; classé au titre immeuble") # mis de côté
-    create(:objet, palissy_PROT: "inscrit au titre objet ; classé au titre objet") # mis de côté
-    create(:objet, palissy_PROT: "inscrit au titre objet : classé au titre objet") # mis de côté
-    create(:objet, palissy_PROT: "inscrit au titre objet ; classé au titre objet ; classé au titre objet") # mis de côté
-    create(:objet, palissy_PROT: "inscrit au titre objet ; classé au titre objet partiellement") # mis de côté
-    create(:objet, palissy_PROT: "Inscrit au titre objet") # inscrit
-    create(:objet, palissy_PROT: "inscrit au titre objet ; inscrit au titre immeuble") # inscrit
-    create(:objet, palissy_PROT: "inscrit au titre objet ; inscrit au titre objet") # inscrit
-    create(:objet, palissy_PROT: "inscrit au titre objet ; inscrit au titre objet partiellement (?)") # inscrit
-    create(:objet, palissy_PROT: "inscrit au titre objet partiellement") # inscrit
-    create(:objet, palissy_PROT: "inscrit au titre objet partiellement ; classé au titre objet partiellement") # mis de côté
-    create(:objet, palissy_PROT: "inscrite au titre objet") # inscrit
-    create(:objet, palissy_PROT: "inscrit MH") # inscrit
-    create(:objet, palissy_PROT: "non protégé") # mis de côté
-    create(:objet, palissy_PROT: "non protégé ?") # mis de côté
-    create(:objet, palissy_PROT: "sans protection") # mis de côté
+    objets_considérés_comme_classés = [
+      "classé au ti",
+      "classé au titre",
+      "classé au titre ensemble historique mobilier",
+      "classé au titre immeuble",
+      "classé au titre immeuble ; classé au titre objet",
+      "classé au titre immeuble ; classé au titre objet ; classé au titre objet",
+      "classé  au titre objet",
+      "classé au  titre objet",
+      "classé au titre objet",
+      "classé au titre objet ",
+      "classé au titre objet (?)",
+      "classé au titre objet.",
+      "Classé au titre objet",
+      "classé au titre objet ; classé au titre immeuble",
+      "classé au titre objet ; classé au titre objet",
+      "classé au titre objet ; classé au titre objet ; classé au titre objet",
+      "classé au titre objet partiellement",
+      "classé comme ensemble historique mobilier",
+      "classé comme ensemble historique mobilier",
+      "classé MH",
+      "classé MH ; classé au titre objet",
+      "classé Monument Historique"
+    ]
+    objets_considérés_comme_inscrits = [
+      "1993/01/11 : inscrit au titre objet",
+      "inscr",
+      "inscri",
+      "inscrit",
+      "inscrit au",
+      "inscrit au ",
+      "inscrit au t",
+      "inscrit au tit",
+      "inscrit au titr",
+      "inscrit au titre",
+      "inscrit au titre des Monuments historiques",
+      "inscrit au titre immeuble",
+      "inscrit au titre obejt",
+      "inscrit au titre obje",
+      " inscrit au titre objet",
+      "\ninscrit au titre objet",
+      "inscrit au titre objet",
+      "inscrit au titre objet (?)",
+      "Inscrit au titre objet",
+      "inscrit au titre objet ; inscrit au titre immeuble",
+      "inscrit au titre objet ; inscrit au titre objet",
+      "inscrit au titre objet ; inscrit au titre objet partiellement (?)",
+      "inscrit au titre objet partiellement",
+      "inscrite au titre objet",
+      "inscrit MH"
+    ]
+    objets_mis_à_part = [
+      "classé au titre immeuble ; inscrit au titre objet",
+      "classé au titre objet ; classé au titre objet ; inscrit au titre objet",
+      "classé au titre objet ; inscrit au titre immeuble",
+      "classé au titre objet ; inscrit au titre objet",
+      "classé au titre objet ; inscrit au titre objet partiellement",
+      "déclassé au titre objet",
+      "désinscrit",
+      "devenu non protégé",
+      "fleur",
+      "i",
+      "immeuble par nature",
+      "inscrit au titre objet ; classé au titre immeuble",
+      "inscrit au titre objet ; classé au titre objet",
+      "inscrit au titre objet : classé au titre objet",
+      "inscrit au titre objet ; classé au titre objet ; classé au titre objet",
+      "inscrit au titre objet ; classé au titre objet partiellement",
+      "inscrit au titre objet ; classé au titre objet partiellement",
+      "inscrit au titre objet partiellement ; classé au titre objet partiellement",
+      "non protégé",
+      "non protégé ?",
+      "sans protection"
+    ]
+    tous_les_objets = objets_considérés_comme_classés + objets_considérés_comme_inscrits + objets_mis_à_part
 
-    expect(Objet.classés.count).to eq(22)
-    expect(Objet.inscrits.count).to eq(26)
+    build_list(:objet, tous_les_objets.size) do |objet, index|
+      objet.palissy_PROT = tous_les_objets[index]
+      objet.save
+    end
+
+    expect(Objet.classés.count).to eq(objets_considérés_comme_classés.size)
+    expect(Objet.inscrits.count).to eq(objets_considérés_comme_inscrits.size)
   end
 end
