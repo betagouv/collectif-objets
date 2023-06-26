064d9294cca3195a84ac1e194cf94815ffb7b284
Brice Durand <brice.durand2@gmail.com>
2023-06-26 18:45:55 +0200

    Affichage du nombre d'objets classés et inscrits sur la page de génération des bordereaux

--- app/helpers/edifice_helper.rb
+++ app/helpers/edifice_helper.rb
@@ -2,10 +2,26 @@
 
 module EdificeHelper
   def nombre_objets_classés_et_inscrits(edifice)
-    if edifice.objets.classés.count > 1
-      "#{edifice.objets.classés.count} objets classés"
-    else
-      "1 objet classé"
-    end
+    nombre_objets_classés = case edifice.objets.classés.count
+                            when 0
+                              ""
+                            when 1
+                              "1 objet classé"
+                            else
+                              "#{edifice.objets.classés.count} objets classés"
+                            end
+
+    nombre_objets_inscrits = case edifice.objets.inscrits.count
+                             when 0
+                               ""
+                             when 1
+                               "1 objet inscrit"
+                             else
+                               "#{edifice.objets.inscrits.count} objets inscrits"
+                             end
+
+    liasion = nombre_objets_classés.present? && nombre_objets_inscrits.present? ? " et " : ""
+
+    nombre_objets_classés + liasion + nombre_objets_inscrits
   end
 end
