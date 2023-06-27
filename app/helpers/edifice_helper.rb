29e4f28d05ac69de37a3e9be6277e42986bf8577
Brice Durand <brice.durand2@gmail.com>
2023-06-27 15:47:10 +0200

    Affichage du nombre d'objets protégés par édifice sur la page des bordereaux

--- app/helpers/edifice_helper.rb
+++ app/helpers/edifice_helper.rb
@@ -1,27 +1,15 @@
 # frozen_string_literal: true
 
 module EdificeHelper
-  def nombre_objets_classés_et_inscrits(edifice)
-    nombre_objets_classés = case edifice.objets.classés.count
-                            when 0
-                              ""
-                            when 1
-                              "1 objet classé"
-                            else
-                              "#{edifice.objets.classés.count} objets classés"
-                            end
-
-    nombre_objets_inscrits = case edifice.objets.inscrits.count
-                             when 0
-                               ""
-                             when 1
-                               "1 objet inscrit"
-                             else
-                               "#{edifice.objets.inscrits.count} objets inscrits"
-                             end
-
-    liasion = nombre_objets_classés.present? && nombre_objets_inscrits.present? ? " et " : ""
-
-    nombre_objets_classés + liasion + nombre_objets_inscrits
+  def texte_nombre_objets_protégés(edifice)
+    nombre_objets_protégés = edifice.objets.protégés.count
+    case nombre_objets_protégés
+    when 0
+      "Aucun objet protégé"
+    when 1
+      "1 objet protégé"
+    else
+      "#{nombre_objets_protégés} objets protégés"
+    end
   end
 end
