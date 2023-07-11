0c3777f0379d3d611c8b020fe274558766a5784f
Brice Durand <brice.durand2@gmail.com>
2023-07-11 17:34:06 +0200

    Harmonisation des noms des compteurs d'objets

--- app/models/concerns/dossiers/include_counts_concern.rb
+++ app/models/concerns/dossiers/include_counts_concern.rb
@@ -11,16 +11,16 @@ module Dossiers
       def self.include_objets_count
         joins(%{
           LEFT JOIN (
-            SELECT communes.id, COUNT(*) AS nombre_objets
+            SELECT communes.id, COUNT(*) AS objets_count
               FROM communes
               INNER JOIN objets ON communes.code_insee = "objets"."palissy_INSEE"
               GROUP BY communes.id
           ) AS nombre_objets_par_commune
           ON nombre_objets_par_commune.id = dossiers.commune_id
-        }).select("COALESCE(nombre_objets, 0) AS nombre_objets")
+        }).select("COALESCE(objets_count, 0) AS objets_count")
       end
 
-      ransacker(:nombre_objets) { Arel.sql("nombre_objets") }
+      ransacker(:nombre_objets) { Arel.sql("objets_count") }
     end
   end
 end
