3c003c76d922f404698389fcaa8e74ca93903bae
Brice Durand <brice.durand2@gmail.com>
2023-07-12 09:00:18 +0200

    En cours : récupération des bonnes données pour l'affichage des fiches

--- app/controllers/conservateurs/fiches_controller.rb
+++ app/controllers/conservateurs/fiches_controller.rb
@@ -4,6 +4,12 @@ module Conservateurs
   class FichesController < BaseController
     def index
       @dossiers = policy_scope(Dossier).where(conservateur: current_conservateur)
+      .joins(:recensements).select("recensements.analyse_fiches").where("cardinality(recensements.analyse_fiches) > 0")
+        .joins(:commune)
+        # .includes(:commune)
+        .select("dossiers.*, communes.nom AS nom_commune, communes.id")
+        .include_objets_count
+        .order(:nom_commune)
     end
 
     private
