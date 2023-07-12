438eed42094bd25c9d8c12464dac7cb06eb0707e
Brice Durand <brice.durand2@gmail.com>
2023-07-12 12:19:15 +0200

    Récupération des communes concernées par les fiches avec le compteur d'objets

--- app/controllers/conservateurs/fiches_controller.rb
+++ app/controllers/conservateurs/fiches_controller.rb
@@ -3,13 +3,11 @@
 module Conservateurs
   class FichesController < BaseController
     def index
-      @dossiers = policy_scope(Dossier).where(conservateur: current_conservateur)
-      .joins(:recensements).select("recensements.analyse_fiches").where("cardinality(recensements.analyse_fiches) > 0")
-        .joins(:commune)
-        # .includes(:commune)
-        .select("dossiers.*, communes.nom AS nom_commune, communes.id")
+      @communes = policy_scope(Commune)
+        .joins(:recensements).where("cardinality(recensements.analyse_fiches) > 0")
         .include_objets_count
-        .order(:nom_commune)
+        .group(:id, "objets_count")
+        .order(:nom)
     end
 
     private
