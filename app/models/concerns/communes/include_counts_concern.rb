47188c8c5d84feccd276c0da33763cf903b58640
Brice Durand <brice.durand2@gmail.com>
2023-08-30 14:58:46 +0200

    Notion de statut global du recensement et de l'analyse d'une commune

--- app/models/concerns/communes/include_counts_concern.rb
+++ app/models/concerns/communes/include_counts_concern.rb
@@ -82,6 +82,34 @@ module Communes
 
       ransacker(:en_peril_count) { Arel.sql("en_peril_count") }
       ransacker(:disparus_count) { Arel.sql("disparus_count") }
+
+      def self.include_statut_global
+        joins(%{
+          LEFT OUTER JOIN (
+            SELECT communes.code_insee, (CASE
+                WHEN communes.status = 'inactive' THEN 'inactive'
+                WHEN communes.status = 'started' THEN 'started'
+                WHEN dossiers.status = 'submitted' AND recensements_analysed_count = 0 THEN 'submitted'
+                WHEN dossiers.status = 'submitted' AND recensements_analysed_count > 0 THEN 'analyse_started'
+                WHEN dossiers.status = 'accepted' then 'accepted'
+              END) AS statut_global
+            FROM communes
+            LEFT OUTER JOIN dossiers
+            ON communes.dossier_id = dossiers.id
+            LEFT OUTER JOIN (
+              SELECT dossiers.id,
+                SUM(CASE WHEN recensements.analysed_at IS NOT NULL THEN 1 ELSE 0 END) AS recensements_analysed_count
+              FROM dossiers
+              LEFT OUTER JOIN recensements
+              ON dossiers.id = recensements.dossier_id
+              GROUP BY dossiers.id
+            ) AS nb_recensements_par_dossiers
+            ON dossiers.id = nb_recensements_par_dossiers.id
+          ) AS communes_statut_global
+          ON communes.code_insee = communes_statut_global.code_insee
+        }.squish)
+        .select("statut_global")
+      end
     end
   end
 end
