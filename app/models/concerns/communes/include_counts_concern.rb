769f196d2c255cd364f8a83a7177c2008ef8c805
Brice Durand <brice.durand2@gmail.com>
2023-09-11 10:21:50 +0200

    Suppression de code mort

--- app/models/concerns/communes/include_counts_concern.rb
+++ app/models/concerns/communes/include_counts_concern.rb
@@ -21,44 +21,6 @@ module Communes
 
       ransacker(:objets_count) { Arel.sql("objets_count") }
 
-      # Objets recensés sur CO
-      def self.include_objets_recenses_count
-        joins(
-          %{
-          LEFT OUTER JOIN (
-            SELECT "palissy_INSEE", COUNT(*) objets_recenses_count
-            FROM objets
-            WHERE exists (SELECT id FROM recensements WHERE recensements.objet_id = objets.id)
-            GROUP BY "palissy_INSEE"
-          ) b ON b."palissy_INSEE" = communes.code_insee
-
-          LEFT OUTER JOIN (
-            SELECT objets."palissy_INSEE", COUNT(*) recensements_analysed_count
-            FROM recensements
-            INNER JOIN objets ON objets.id = recensements.objet_id
-            WHERE recensements.analysed_at IS NOT NULL
-            GROUP BY objets."palissy_INSEE"
-          ) c ON c."palissy_INSEE" = communes.code_insee
-
-          LEFT OUTER JOIN (
-            SELECT objets."palissy_INSEE", COUNT(*) recensements_prioritaires_count
-            FROM recensements
-            INNER JOIN objets ON objets.id = recensements.objet_id
-            WHERE (#{Recensement::RECENSEMENT_PRIORITAIRE_SQL})
-            GROUP BY objets."palissy_INSEE"
-          ) d ON d."palissy_INSEE" = communes.code_insee
-        }
-        ).select(
-          "communes.*," \
-          "COALESCE(b.objets_recenses_count, 0) AS objets_recenses_count, " \
-          "(COALESCE(100 * b.objets_recenses_count, 0)::float / COALESCE(a.objets_count, 1)) " \
-          "AS objets_recenses_percentage, " \
-          "(COALESCE(100 * c.recensements_analysed_count, 0)::float / COALESCE(a.objets_count, 1)) " \
-          "AS recensements_analysed_percentage," \
-          "COALESCE(d.recensements_prioritaires_count, 0) as recensements_prioritaires_count"
-        )
-      end
-
       # Objets recensés et prioritaires d'une commune
       def self.include_recensements_prioritaires_count
         joins(
