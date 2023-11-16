e3cf2433c2e1c6a8b565d2ec6d7171d2081bc3f0
Brice Durand <brice.durand2@gmail.com>
2023-11-16 09:47:39 +0100

    build: ajout de la commande de migration de base de données dans le script de post déploiement

--- scripts/postdeploy.sh
+++ scripts/postdeploy.sh
@@ -3,3 +3,4 @@ srcdir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )
 
 source "$srcdir/create_sentry_release.sh"
 rails runner "$srcdir/trigger_raise_on_purpose.rb"
+bundle exec rails db:migrate
