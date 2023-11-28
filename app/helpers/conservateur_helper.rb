b838c1e6e7a2d51c97e710c9223e6c8254d4d5ba
Brice Durand <brice.durand2@gmail.com>
2023-11-28 16:17:34 +0100

    wording: onglet Recensement devient Objets

--- app/helpers/conservateur_helper.rb
+++ app/helpers/conservateur_helper.rb
@@ -4,7 +4,7 @@ module ConservateurHelper
   def conservateur_commune_tabs(commune)
     tab = Data.define(:key, :title, :path)
     [
-      tab.new(:analyse, "Recensement", conservateurs_commune_path(commune)),
+      tab.new(:analyse, "Objets", conservateurs_commune_path(commune)),
       tab.new(:messagerie, commune_messagerie_title(commune), conservateurs_commune_messages_path(commune)),
       tab.new(:rapport, "Examen", conservateurs_commune_dossier_path(commune)),
       tab.new(:bordereau, "Bordereaux de récolement", new_conservateurs_commune_bordereau_path(commune))
