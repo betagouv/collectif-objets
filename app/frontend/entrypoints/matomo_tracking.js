f82e120b9533c725b44bb7c81e6003e39c8e6894
Adrien Di Pasquale <adrien.di_pasquale@beta.gouv.fr>
2023-12-11 11:13:25 +0100

    migration du matomo de data.gouv vers beta.gouv (#1028)

--- app/frontend/entrypoints/matomo_tracking.js
+++ app/frontend/entrypoints/matomo_tracking.js
@@ -3,10 +3,10 @@ var _paq = window._paq = window._paq || [];
 _paq.push(['disableCookies']);
 _paq.push(['trackPageView']);
 _paq.push(['enableLinkTracking']);
-(function() {
-  var u="https://stats.data.gouv.fr/";
-  _paq.push(['setTrackerUrl', u+'piwik.php']);
-  _paq.push(['setSiteId', '237']);
-  var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-  g.async=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+(function () {
+  var u = "https://stats.beta.gouv.fr/";
+  _paq.push(['setTrackerUrl', u + 'matomo.php']);
+  _paq.push(['setSiteId', '71']);
+  var d = document, g = d.createElement('script'), s = d.getElementsByTagName('script')[0];
+  g.async = true; g.src = u + 'matomo.js'; s.parentNode.insertBefore(g, s);
 })();
