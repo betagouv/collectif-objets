f73c3552e9191dcbc71474cf48d4d7a3505ae0fa
Brice Durand <brice.durand2@gmail.com>
2024-01-09 12:23:33 +0100

    Usage de méthode bang pour éviter les erreurs silencieuses

--- db/migrate/20240108153647_fix_dossier_with_submitted_at_nil.rb
+++ db/migrate/20240108153647_fix_dossier_with_submitted_at_nil.rb
@@ -1,7 +1,7 @@
 class FixDossierWithSubmittedAtNil < ActiveRecord::Migration[7.1]
   def up
     Commune.joins(:dossier).where(status: :completed).where(dossier: { submitted_at: nil }).find_each do |commune|
-      commune.dossier.update(submitted_at: commune.completed_at)
+      commune.dossier.update!(submitted_at: commune.completed_at)
       puts "Mise à jour du dossier de la commune #{commune.nom} - code insee #{commune.code_insee} - id #{commune.id} avec un submitted_at à #{commune.dossier.submitted_at}"
     end
   end
