e2b8176b3631d718176b8bb17a8440a0f956b899
Adrien Di Pasquale <adrien.di_pasquale@beta.gouv.fr>
2024-01-23 11:20:32 +0100

    disable postgis in db (#1135)

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_23_081828) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_23_094037) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-  enable_extension "postgis"
   enable_extension "unaccent"
 
   create_table "active_storage_attachments", force: :cascade do |t|
