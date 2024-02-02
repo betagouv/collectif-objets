a3898cfbd6c707a50e3c995868b18fa9508bcbef
Brice Durand <brice@agencezero.com>
2024-02-02 09:36:24 +0100

    feature: fusion étapes localisation: wip fusion étapes localisation

--- app/models/recensement_wizard/step1.rb
+++ app/models/recensement_wizard/step1.rb
@@ -3,35 +3,36 @@
 module RecensementWizard
   class Step1 < Base
     STEP_NUMBER = 1
-    TITLE = "Recherche"
+    TITLE = "Localisation"
 
-    attr_accessor :investigation, :confirmation_introuvable
+    attr_accessor :localisation, :confirmation_introuvable
 
-    validates :investigation,
-              inclusion: { in: %w[confirmation_sur_place introuvable],
-                           message: "Veuillez choisir une option" }
+    validates :localisation, presence: { message: "Veuillez préciser où se trouve l’objet" }
+
+    validates :localisation,
+              inclusion: {
+                in: Recensement::LOCALISATIONS,
+                message: "La localisation de l’objet n’est pas valide"
+              },
+              if: -> { localisation.present? }
 
     def initialize(recensement)
       super
       self.confirmation_introuvable = recensement.absent? ? "true" : "false"
-      if recensement.absent?
-        self.investigation = "introuvable"
-      elsif recensement.confirmation_sur_place?
-        self.investigation = "confirmation_sur_place"
-      end
+      # self.localisation = recensement.localisation
     end
 
-    def permitted_params = %i[investigation confirmation_introuvable]
+    def permitted_params = %i[localisation confirmation_introuvable]
 
     def next_step_number
-      investigation == "introuvable" ? 5 : super
+      localisation == Recensement::LOCALISATION_ABSENT ? 5 : super
     end
 
     def confirmation_modal_path_params
-      return if investigation != "introuvable" || confirmation_introuvable
+      return if localisation != Recensement::LOCALISATION_ABSENT || confirmation_introuvable
 
       { modal: "confirmation-introuvable",
-        wizard: { investigation: "introuvable", confirmation_introuvable: "true" } }
+        wizard: { localisation: Recensement::LOCALISATION_ABSENT, confirmation_introuvable: "true" } }
     end
 
     def confirmation_modal_close_path
@@ -40,22 +41,14 @@ module RecensementWizard
 
     def assign_attributes(attributes)
       super
-      if investigation == "confirmation_sur_place"
-        recensement.confirmation_sur_place = true
-        if recensement.absent?
-          recensement.recensable = nil
-          recensement.localisation = nil
-          recensement.status = "draft" # force returning to draft for completed ones
-        end
-      elsif investigation == "introuvable" && confirmation_introuvable
-        recensement.confirmation_sur_place = true
-        recensement.localisation = Recensement::LOCALISATION_ABSENT
-        recensement.recensable = false
-        recensement.edifice_nom = nil
-        recensement.etat_sanitaire = nil
-        recensement.securisation = nil
-        recensement.photos = []
-      end
+      return unless localisation == Recensement::LOCALISATION_ABSENT && confirmation_introuvable
+
+      recensement.localisation = Recensement::LOCALISATION_ABSENT
+      recensement.recensable = false
+      recensement.edifice_nom = nil
+      recensement.etat_sanitaire = nil
+      recensement.securisation = nil
+      recensement.photos = []
     end
   end
 end
