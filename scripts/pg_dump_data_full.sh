40f0cc2324137524f8c37961faad93b95da22493
Adrien Di Pasquale <adrien@dipasquale.fr>
2024-02-26 18:41:28 +0100

    devops: stop dumping good_job tables [skip-ci]

--- scripts/pg_dump_data_full.sh
+++ scripts/pg_dump_data_full.sh
@@ -4,5 +4,10 @@ pg_dump \
   --format c \
   --exclude-table ar_internal_metadata \
   --exclude-table schema_migrations \
+  --exclude-table good_jobs \
+  --exclude-table good_job_settings \
+  --exclude-table good_job_processes \
+  --exclude-table good_job_executions \
+  --exclude-table good_job_batches \
   -f $2 \
   $1
