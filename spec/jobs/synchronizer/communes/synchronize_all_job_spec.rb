616e075c1b4b07951f000ff416797880ce77116f
Adrien Di Pasquale <adrien.di_pasquale@beta.gouv.fr>
2024-02-27 10:10:57 +0100

    2. petits refactors sur Synchronizer::Communes::SynchronizeAllJob (#1180)
    - utiliser `Synchronizer::Communes::Row#code_insee` plutôt que d’accéder à la valeur brute dans la csv_row `commune_code_insee`
    - utiliser `out_of_scope?` plutôt que `invalid?`

--- spec/jobs/synchronizer/communes/synchronize_all_job_spec.rb
+++ spec/jobs/synchronizer/communes/synchronize_all_job_spec.rb
@@ -15,7 +15,9 @@ module Synchronizer
         rows_code_insees.map { |code_insee| { "code_insee_commune" => code_insee } }
       end
       let(:rows) do
-        csv_rows.map { instance_double(Row, invalid?: false) }
+        rows_code_insees.zip(rows_in_scope).map do |code_insee, in_scope|
+          instance_double(Row, out_of_scope?: !in_scope, in_scope?: in_scope, code_insee:)
+        end
       end
       let(:logger) { instance_double(Synchronizer::Logger, log: nil, close: nil) }
       let(:api_client) { instance_double(ApiClientAnnuaireAdministration, count_all: csv_rows.count) }
@@ -26,10 +28,9 @@ module Synchronizer
         expect(api_client).to receive(:each) do |&block|
           csv_rows.each { block.call(_1) }
         end
-        expect(api_client).to receive(:each_slice).exactly(:twice).and_yield(csv_rows)
+        expect(api_client).to receive(:each_slice).at_least(:once).and_yield(csv_rows)
         csv_rows.count.times do |i|
-          expect(Row).to receive(:new).with(csv_rows[i]).and_return(rows[i])
-          expect(rows[i]).to receive(:[]).with("code_insee_commune").and_return(csv_rows[i]["code_insee_commune"])
+          allow(Row).to receive(:new).at_least(:once).with(csv_rows[i]).and_return(rows[i])
         end
         expect(Synchronizer::Logger).to receive(:new).and_return(logger)
         expect(batch).to receive(:synchronize).with(if_block: anything).twice
@@ -37,6 +38,7 @@ module Synchronizer
 
       context "simple case" do
         let(:rows_code_insees) { %w[01002 03001 97201] }
+        let(:rows_in_scope) { [true, true, true] }
         it "should work" do
           expect(Batch::Base).to receive(:new).exactly(:twice).with(csv_rows, logger:).and_return(batch)
           SynchronizeAllJob.new.perform
@@ -45,9 +47,23 @@ module Synchronizer
 
       context "when there are multiple mairies principales for a single code insee" do
         let(:rows_code_insees) { %w[01002 03001 97201 97201 13001] }
+        let(:rows_in_scope) { [true, true, true, true, true] }
         it "should exclude these lines" do
           expect(Batch::Base).to \
             receive(:new).exactly(:twice).with([csv_rows[0], csv_rows[1], csv_rows[4]], logger:).and_return(batch)
+          SynchronizeAllJob.new.perform
+        end
+      end
+
+      context "when there are multiple rows for a single code insee but one is out of scope" do
+        let(:rows_code_insees) { %w[01002 03001 97201 97201 13001] }
+        let(:rows_in_scope) { [true, true, true, false, true] }
+        it "should not exclude the duplicate rows, the batch will exclude the out of scope one itself" do
+          expect(Batch::Base).to \
+            receive(:new)
+              .exactly(:twice)
+              .with(csv_rows, logger:)
+              .and_return(batch)
           SynchronizeAllJob.new.perform
         end
       end
