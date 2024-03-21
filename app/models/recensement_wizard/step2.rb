b152dc8f8ee3079778e90228ffbac1d7fa0cb927
Brice Durand <brice@agencezero.com>
2024-03-21 17:18:36 +0100

    Meilleure gestion des erreurs sur le choix des édifices

--- app/models/recensement_wizard/step2.rb
+++ app/models/recensement_wizard/step2.rb
@@ -5,18 +5,25 @@ module RecensementWizard
     STEP_NUMBER = 2
     TITLE = "Précisions sur la localisation"
 
+    validates :edifice_id,
+              presence: { message: "Veuillez sélectionner un édifice ou l'option Autre édifice." }
+
     validates \
       :edifice_nom,
       presence: {
         message: "Veuillez préciser le nom de l’édifice dans lequel l’objet a été déplacé"
-      }, unless: -> { edifice_id.present? }
+      }, if: -> { autre_edifice? }
 
     def permitted_params = %i[edifice_id edifice_nom]
 
     def assign_attributes(attributes)
       super
 
-      recensement.edifice_nom = nil if edifice_id.present?
+      recensement.edifice_nom = nil unless autre_edifice?
+    end
+
+    def autre_edifice?
+      edifice_id.present? && edifice_id.zero?
     end
   end
 end
