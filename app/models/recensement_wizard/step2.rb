f64f907eedad92c0e4d6ee6e88f65bf9f22bba66
Brice Durand <brice@agencezero.com>
2024-03-25 22:47:54 +0100

    gestion des retours arrière, erreurs et initialisation correcte de la précision sur la localisation

--- app/models/recensement_wizard/step2.rb
+++ app/models/recensement_wizard/step2.rb
@@ -9,20 +9,40 @@ module RecensementWizard
 
     validates :edifice_id,
               presence: { message: "Veuillez sélectionner un édifice ou \
-                cocher la case ci-dessous si l'édifice n'est pas dans la liste." }
+                cocher la case ci-dessous si l'édifice n'est pas dans la liste." },
+              unless: -> { autre_edifice_checked }
 
     validates \
       :edifice_nom,
       presence: {
         message: "Veuillez préciser le nom de l’édifice dans lequel l’objet a été déplacé"
-      }, if: -> { autre_edifice? }
+      }, if: -> { autre_edifice_checked }
 
     def permitted_params = %i[edifice_id edifice_nom autre_edifice_checked]
 
+    def initialize(recensement)
+      super
+      self.autre_edifice_checked = recensement.edifice_nom.present?
+    end
+
     def assign_attributes(attributes)
+      if attributes[:edifice_id].present? && attributes[:edifice_id] == "0"
+        attributes[:autre_edifice_checked] = true
+        attributes.delete(:edifice_id)
+      end
+
+      if attributes[:edifice_id].present? && attributes[:edifice_id] != "0"
+        attributes[:autre_edifice_checked] = false
+        attributes.delete(:edifice_nom)
+      end
+
       super
 
-      recensement.edifice_nom = nil unless autre_edifice?
+      if autre_edifice_checked
+        recensement.edifice_id = nil
+      else
+        recensement.edifice_nom = nil
+      end
     end
 
     def autre_edifice?
