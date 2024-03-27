043e63a367e07634653006e13cd32194d105d7d0
Brice Durand <brice@agencezero.com>
2024-03-27 10:40:11 +0100

    masquage dynamique du champ pour le nom de l'édifice quand Autre édifice est sélectionné

--- app/frontend/stimulus_controllers/choix_edifice_controller.js
+++ app/frontend/stimulus_controllers/choix_edifice_controller.js
@@ -1,14 +1,13 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = [ "edificeNom", "edificeId", "autreEdificeCheckbox" ]
+  static targets = [ "edificeNom", "autreEdifice" ]
 
   connect() {
     this.check_autre_edifice();
   }
 
   check_autre_edifice() {
-    this.edificeIdTarget.disabled = this.autreEdificeCheckboxTarget.checked;
-    this.edificeNomTarget.style.display = this.autreEdificeCheckboxTarget.checked ? "block" : "none";
+    this.edificeNomTarget.style.display = this.autreEdificeTarget.selected ? "block" : "none";
   }
 }
