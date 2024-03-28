4ddbeefa35f8d346736324b48a4b329610649db5
Brice Durand <brice@agencezero.com>
2024-03-28 11:16:36 +0100

    Fusion des étapes accessibilité et photos en étape 3

--- app/models/recensement_wizard/step3.rb
+++ app/models/recensement_wizard/step3.rb
@@ -3,7 +3,7 @@
 module RecensementWizard
   class Step3 < Base
     STEP_NUMBER = 3
-    TITLE = "Accessibilité"
+    TITLE = "Photos de l'objet"
 
     include ActiveStorageValidations
 
@@ -16,12 +16,18 @@ module RecensementWizard
         message: "Veuillez renseigner si l’objet est recensable ou non"
       }
 
+    validates(
+      :photos,
+      content_type: %w[image/jpg image/jpeg image/png],
+      size: { less_than: 20.megabytes }
+    )
+
     def initialize(recensement)
       super
       self.confirmation_not_recensable = recensement.recensable_was == false ? "true" : "false"
     end
 
-    def permitted_params = %i[recensable confirmation_not_recensable]
+    def permitted_params = %i[recensable confirmation_not_recensable photos]
 
     def confirmation_modal_path_params
       return if recensable != false || confirmation_not_recensable
@@ -34,8 +40,16 @@ module RecensementWizard
       recensable == false ? 6 : super
     end
 
+    # rubocop:disable Metrics/CyclomaticComplexity
     def assign_attributes(attributes)
+      # not sure this is useful considering we do upload photos directly from the client
+      if attributes[:photos]&.any?
+        recensement.photos = recensement.photos.map(&:signed_id) + [attributes[:photos][0]]
+        recensement.photos_count += 1
+      end
+
       super
+
       if recensable == false && confirmation_not_recensable
         recensement.etat_sanitaire = nil
         recensement.securisation = nil
@@ -44,5 +58,6 @@ module RecensementWizard
         recensement.status = "draft"
       end
     end
+    # rubocop:enable Metrics/CyclomaticComplexity
   end
 end
