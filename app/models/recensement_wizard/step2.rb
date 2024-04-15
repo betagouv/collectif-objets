e66e6889456c414b173bbc9c70e4d44914bde237
Brice Durand <brice@agencezero.com>
2024-04-15 18:06:59 +0200

    Ajout du champ pour le code INSEE de la commune où l'objet a été déplacé

--- app/models/recensement_wizard/step2.rb
+++ app/models/recensement_wizard/step2.rb
@@ -11,6 +11,12 @@ module RecensementWizard
         message: "Veuillez préciser le nom de l’édifice dans lequel l’objet a été déplacé"
       }
 
-    def permitted_params = %i[edifice_nom]
+    validates :autre_commune_code_insee,
+              presence: { message: "Veuillez préciser le code INSEE de la commune dans laquelle l'objet a été déplacé" }
+
+    validates :autre_commune_code_insee,
+              format: { with: /\b\d{5}\b/, message: "Le code INSEE doit être composé de 5 chiffres" }
+
+    def permitted_params = %i[edifice_nom autre_commune_code_insee]
   end
 end
