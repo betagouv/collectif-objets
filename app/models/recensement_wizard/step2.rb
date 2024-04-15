9a9740858e185f82b8161ec90ed6b39bd71cbbaa
Brice Durand <brice@agencezero.com>
2024-04-15 09:13:27 +0200

    Ajout d'une select box pour choisir la commune lors du signalement d'un déplacement d'objet

--- app/models/recensement_wizard/step2.rb
+++ app/models/recensement_wizard/step2.rb
@@ -5,7 +5,7 @@ module RecensementWizard
     STEP_NUMBER = 2
     TITLE = "Précisions sur la localisation"
 
-    attr_accessor :autre_edifice_checked
+    attr_accessor :autre_edifice_checked, :autre_commune_code_insee
 
     validates :edifice_id,
               presence: { message: "Veuillez sélectionner un édifice ou \
@@ -18,14 +18,25 @@ module RecensementWizard
         message: "Veuillez préciser le nom de l’édifice dans lequel l’objet a été déplacé"
       }, if: -> { autre_edifice_checked }
 
-    def permitted_params = %i[edifice_id edifice_nom autre_edifice_checked]
+    def permitted_params = %i[edifice_id edifice_nom autre_edifice_checked autre_commune_code_insee]
 
     def initialize(recensement)
       super
       self.autre_edifice_checked = recensement.edifice_nom.present?
+      self.autre_commune_code_insee ||= recensement.commune.code_insee
+    end
+
+    def next_step_number
+      edifice_id.present? || edifice_nom.present? ? super : step_number
     end
 
     def assign_attributes(attributes)
+      # Reset de l'édifice si une autre commune est choisie
+      # if autre_commune_selected?
+      #   attributes.delete(:edifice_id)
+      #   attributes.delete(:edifice_nom)
+      # end
+
       if attributes[:edifice_id].present? && attributes[:edifice_id] == "0"
         attributes[:autre_edifice_checked] = true
         attributes.delete(:edifice_id)
@@ -47,6 +58,10 @@ module RecensementWizard
 
     def autre_edifice?
       edifice_id.present? && edifice_id.zero?
+    end
+
+    def autre_commune_selected?
+      autre_commune_code_insee != commune.code_insee
     end
   end
 end
