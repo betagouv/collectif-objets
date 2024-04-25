36b30f4daf6e97ca49712a1a3ca24a4a77b186e7
Brice Durand <brice@agencezero.com>
2024-04-25 15:48:25 +0200

    cleanup: suppression de code mort

--- app/frontend/stimulus_controllers/choix_edifice_controller.js
+++ app/frontend/stimulus_controllers/choix_edifice_controller.js
@@ -3,18 +3,9 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = [ "edificeNomFieldset", "edificeNomInput", "autreEdifice" ]
 
-  // connect() {
-  //   this.check_autre_edifice();
-  // }
-
   check_autre_edifice() {
     this.edificeNomFieldsetTarget.classList.toggle("fr-hidden");
     if (this.autreEdificeTarget.selected)
       this.edificeNomInputTarget.value = "";
   }
-
-  autre_commune_selected(event) {
-    // event.currentTarget.closest("form").requestSubmit();
-  }
-
 }
