968e957e0c3c7d7710b73f7fc7610d0ebc2093dc
Brice Durand <brice@agencezero.com>
2024-05-16 10:06:51 +0200

    db: migration de rollback pour le déplacement d'objets

--- db/migrate/20240415152223_add_autre_commune_code_insee_to_recensements.rb
+++ db/migrate/20240415152223_add_autre_commune_code_insee_to_recensements.rb
@@ -1,5 +1,12 @@
 class AddAutreCommuneCodeInseeToRecensements < ActiveRecord::Migration[7.1]
-  def change
+  def up
     add_column :recensements, :autre_commune_code_insee, :string
+  end
+
+  def down
+    remove_column :recensements, :autre_commune_code_insee, :string
+
+    Recensement.where(localisation: "deplacement_autre_commune").update_all(localisation: "autre_edifice")
+    Recensement.where(localisation: "deplacement_temporaire").update_all(localisation: "absent")
   end
 end
