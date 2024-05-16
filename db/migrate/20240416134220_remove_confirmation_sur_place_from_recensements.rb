9a1472016da2d7b25dc548692a51dfbf033d5f4c
Brice Durand <brice@agencezero.com>
2024-05-16 10:36:14 +0200

    db: migration de rollback suite à la fusion des questions introuvable et localisaiton du formulaire de recensement

--- db/migrate/20240416134220_remove_confirmation_sur_place_from_recensements.rb
+++ db/migrate/20240416134220_remove_confirmation_sur_place_from_recensements.rb
@@ -1,5 +1,13 @@
 class RemoveConfirmationSurPlaceFromRecensements < ActiveRecord::Migration[7.1]
-  def change
+  def up
     remove_column :recensements, :confirmation_sur_place, :boolean
+  end
+
+  def down
+    add_column :recensements, :confirmation_sur_place, :boolean
+
+    execute <<-SQL.squish
+      UPDATE recensements SET confirmation_sur_place = true
+    SQL
   end
 end
