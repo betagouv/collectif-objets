cb90e6ad7234c819b177895654811dab963ea87b
Goulven Champenois <goulven.champenois@beta.gouv.fr>
2024-05-17 11:54:12 +0200

    Simplification de l'affichage du guide de recensement
    Corrige l'erreur Sentry #52860 (825 occurrences)

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 class PagesController < ApplicationController
-  PDFS = { "guide" => "Guidederecensement" }.freeze
   STATIC_FILES_HOST = "fichiers.collectif-objets.beta.gouv.fr"
 
   before_action :render_markdown, only: [
@@ -24,32 +23,21 @@ class PagesController < ApplicationController
   end
 
   def guide
-    params[:pdf] = "guide"
-    pdf_embed
-    render "pdf_embed"
-  end
+    respond_to do |format|
+      format.html
+      format.pdf do
+        filename = "Guidederecensement"
+        pdf_body = Rails.cache.fetch("pdfs/1.0/#{filename}.pdf", expires_in: 2.days) do
+          url = "https://#{STATIC_FILES_HOST}/#{filename}.pdf"
+          res = Net::HTTP.get_response(URI.parse(url))
+          raise unless res.code.starts_with?("2")
 
-  def pdf_embed
-    @pdf_name = params[:pdf]
-    raise unless PDFS.keys.include?(@pdf_name)
+          res.body
+        end
 
-    @breadcrumbs = @pdf_name.starts_with?("fiche") ? [["Fiches", fiches_url]] : []
-    @page = params[:page]&.to_i if params[:page]&.to_i&.positive?
-  end
-
-  def pdf_download
-    filename = PDFS[params[:pdf]]
-    raise if filename.blank?
-
-    pdf_body = Rails.cache.fetch("pdfs/1.0/#{filename}.pdf", expires_in: 2.days) do
-      url = "https://#{STATIC_FILES_HOST}/#{filename}.pdf"
-      res = Net::HTTP.get_response(URI.parse(url))
-      raise unless res.code.starts_with?("2")
-
-      res.body
+        send_data(pdf_body, content_type: "application/pdf", filename:, disposition: :inline)
+      end
     end
-
-    send_data(pdf_body, content_type: "application/pdf", filename:, disposition: :inline)
   end
 
   def campaigns_ics
