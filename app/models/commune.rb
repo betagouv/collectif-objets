eaf3671c03ed30ef227c350d01a0fba9945cbb5d
Brice Durand <brice@agencezero.com>
2024-06-12 17:48:44 +0200

    met à jour le ratio de recensement lorsque la commune repasse en non recensée

--- app/models/commune.rb
+++ app/models/commune.rb
@@ -22,7 +22,7 @@ class Commune < ApplicationRecord
       transitions from: :completed, to: :started
     end
 
-    event :return_to_inactive do
+    event :return_to_inactive, after: :refresh_recensement_ratio do
       transitions from: :completed, to: :inactive
     end
   end
@@ -215,6 +215,10 @@ class Commune < ApplicationRecord
 
   def aasm_after_return_to_started
     dossier.return_to_construction! unless dossier.construction?
+  end
+
+  def refresh_recensement_ratio
+    RefreshCommuneRecensementRatioJob.perform_later(id)
   end
 
   # -------
