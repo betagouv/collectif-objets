1166b8e1b985d925e93a2632f51916f393e7e3d2
Goulven Champenois <goulven.champenois@beta.gouv.fr>
2024-06-12 09:33:42 +0200

    Désactive les logs pendant les tests pour gagner en vitesse
    Les logs de tests ne sont jamais consultés, particulièrement en CI où ils sont immédiatement supprimés.
    https://jetthoughts.com/blog/speed-up-your-rails-test-suite-by-6-in-1-line-testing-ruby/

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -16,6 +16,10 @@ Rails.application.configure do
   # Turn false under Spring and add config.action_view.cache_template_loading = true.
   config.cache_classes = true
 
+  # Disable logging to speed up tests
+  config.logger = Logger.new(nil)
+  config.log_level = :fatal
+
   # Eager loading loads your whole application. When running a single test locally,
   # this probably isn't necessary. It's a good idea to do in a continuous integration
   # system, or in some way before deploying your code.
