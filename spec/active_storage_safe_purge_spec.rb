1966398cc8b8ea6c8b5ad2254afa12a2b98ff32b
Goulven Champenois <goulven.champenois@beta.gouv.fr>
2024-06-12 10:45:44 +0200

    Mock le handshake AWS dans les tests du monkey patch ActiveStorage

--- spec/active_storage_safe_purge_spec.rb
+++ spec/active_storage_safe_purge_spec.rb
@@ -32,6 +32,7 @@ RSpec.describe "Active Storage safe purge monkey patches" do
 
   context "when using production service" do
     let(:blob) { local_blob.tap { _1.update!(service_name: "scaleway_production") } }
+    before { stub_request(:any, /.*/).to_return(status: 200, body: "", headers: {}) }
     it "the blob is not deleted" do
       expect(blob.service).not_to receive(:delete)
       expect(blob.service).not_to receive(:delete_prefixed)
