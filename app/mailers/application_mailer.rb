422b345b87a42e2f3f1e7b3de67d904993cbce5f
Goulven Champenois <goulven.champenois@beta.gouv.fr>
2024-06-14 15:16:21 +0200

    Stocke le nom du projet dans le fichier d'I18n pour éviter les répétitions

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -3,7 +3,7 @@
 class ApplicationMailer < ActionMailer::Base
   include ActionMailer::Html2Text # Generate a text email based on the HTML
 
-  default from: email_address_with_name(CONTACT_EMAIL, "Collectif Objets")
+  default from: email_address_with_name(CONTACT_EMAIL, I18n.t("application_mailer_layout.project_name"))
   layout "application_mailer"
   prepend_view_path "app/mailer_views"
 end
