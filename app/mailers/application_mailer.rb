b943c20812f4ea5997b9258e199c278af452adc7
Goulven Champenois <goulven.champenois@beta.gouv.fr>
2024-06-14 15:12:21 +0200

    Remplace une méthode custom par un helper Rails

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -6,13 +6,4 @@ class ApplicationMailer < ActionMailer::Base
   default from: email_address_with_name(CONTACT_EMAIL, "Collectif Objets")
   layout "application_mailer"
   prepend_view_path "app/mailer_views"
-
-  # rubocop:disable Rails/OutputSafety
-  def line_breaks_to_br(content)
-    return content if content.blank?
-
-    content.gsub("\n", "<br />").html_safe
-  end
-  helper_method :line_breaks_to_br
-  # rubocop:enable Rails/OutputSafety
 end
