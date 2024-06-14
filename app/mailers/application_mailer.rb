4ceb7cdd71e45f6a9e9e40fe415f60dda271001b
Goulven Champenois <goulven.champenois@beta.gouv.fr>
2024-06-14 15:11:46 +0200

    Génère automatiquement la version texte des emails

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class ApplicationMailer < ActionMailer::Base
+  include ActionMailer::Html2Text # Generate a text email based on the HTML
+
   default from: email_address_with_name(CONTACT_EMAIL, "Collectif Objets")
   layout "application_mailer"
   prepend_view_path "app/mailer_views"
