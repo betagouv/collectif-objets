dbc80fabbf61329a45161f8d9b5bb6e1b9c45d0c
Brice Durand <brice@agencezero.com>
2024-06-17 11:26:33 +0200

    suppression de recensement_ratio dans un script

--- scripts/reset_recensements_dossiers_communes.rb
+++ scripts/reset_recensements_dossiers_communes.rb
@@ -4,4 +4,4 @@ raise if Rails.configuration.x.environment_specific_name == "production"
 
 Dossier.delete_all
 Recensement.delete_all
-Commune.update_all(status: :inactive, recensement_ratio: 0, started_at: nil)
+Commune.update_all(status: :inactive, started_at: nil)
