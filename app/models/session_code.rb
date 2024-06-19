b7a94f59fce1df4dc294bd85dc895cedb0017cfb
Goulven Champenois <goulven.champenois@beta.gouv.fr>
2024-06-19 16:18:54 +0200

    Découple le contrôleur de code de connexion du modèle associé
    Les détails d'implémentation ne doivent pas être connus hors de la classe

--- app/models/session_code.rb
+++ app/models/session_code.rb
@@ -5,6 +5,10 @@ class SessionCode < ApplicationRecord
 
   belongs_to :user
 
+  before_create :generate_code
+
+  delegate :random_code, to: :class
+
   def self.random_code
     length = 6
     rand(10.pow(length - 1)...10.pow(length)).to_s
@@ -19,4 +23,8 @@ class SessionCode < ApplicationRecord
   def used? = used_at.present?
 
   def mark_used! = update!(used_at: Time.zone.now)
+
+  private
+
+  def generate_code = self.code = random_code
 end
