0232bf7d39f3e62dbbe1b67b5b1ca0436640d725
Goulven Champenois <goulven.champenois@beta.gouv.fr>
2024-06-19 16:08:19 +0200

    Allonge la durée de validité du code de connexion 1h -> 24h

--- app/models/session_code.rb
+++ app/models/session_code.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class SessionCode < ApplicationRecord
-  EXPIRE_AFTER = 60.minutes.freeze
+  EXPIRE_AFTER = 1.day.freeze
 
   belongs_to :user
 
@@ -13,6 +13,8 @@ class SessionCode < ApplicationRecord
   def expired?
     created_at < Time.zone.now - EXPIRE_AFTER
   end
+
+  def valid_until = (created_at || Time.zone.now) + EXPIRE_AFTER
 
   def used? = used_at.present?
 
