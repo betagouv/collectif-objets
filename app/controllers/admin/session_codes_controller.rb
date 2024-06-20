039020b79161019d0ec4e26fbfac733674db4cf9
Goulven Champenois <goulven.champenois@beta.gouv.fr>
2024-06-20 10:37:37 +0200

    Affichage des statistiques de codes de connexion et derniers codes générés

---
+++ app/controllers/admin/session_codes_controller.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module Admin
+  class SessionCodesController < BaseController
+    def index; end
+
+    private
+
+    def active_nav_links = %w[Admin]
+  end
+end
