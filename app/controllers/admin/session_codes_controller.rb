e2799225ddaa0a552ba03cb9374da050a1cfa193
Goulven Champenois <goulven.champenois@beta.gouv.fr>
2024-06-27 13:08:39 +0200

    Permet de paginer parmi les codes de connexion

--- app/controllers/admin/session_codes_controller.rb
+++ app/controllers/admin/session_codes_controller.rb
@@ -4,6 +4,9 @@ module Admin
   class SessionCodesController < BaseController
     def index
       @total = SessionCode.count
+      @limit = 10
+      @offset = params.fetch(:offset, 0).to_i
+      @session_codes = SessionCode.includes(:commune).order(created_at: :desc).limit(@limit).offset(@offset * @limit)
     end
 
     private
