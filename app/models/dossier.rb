4a4ba4329ab7d5a7b0bf8b74586c3e928081ac43
Brice Durand <brice@agencezero.com>
2024-07-10 12:03:29 +0200

    contrainte d'unicité de commune sur le dossier plus cohérente

--- app/models/dossier.rb
+++ app/models/dossier.rb
@@ -34,7 +34,7 @@ class Dossier < ApplicationRecord
 
   validates :conservateur, presence: true, if: :accepted?
   validates :visit, inclusion: { in: %w[souhaitable prioritaire] }, allow_nil: true
-  validates :commune_id, uniqueness: { conditions: -> { where.not(status: "archived") } }
+  validates :commune_id, uniqueness: true, unless: :archived?
 
   delegate :departement, to: :commune
 
