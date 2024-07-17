053592ce4776af7983540251fafc4419d60bca9e
Goulven Champenois <goulven.champenois@beta.gouv.fr>
2024-07-17 11:09:44 +0200

    Réorganise les colonnes de l'export des objets manquants

--- spec/models/export_mpp_spec.rb
+++ spec/models/export_mpp_spec.rb
@@ -60,9 +60,10 @@ RSpec.describe Exports::Mpp, type: :model do
           objet.palissy_REF,
           objet.departement.nom,
           objet.lieu_actuel_code_insee,
+          objet.recensement.notes,
           objet.recensement.dossier.notes_commune,
           objet.recensement.dossier.notes_conservateur,
-          objet.recensement.notes,
+          objet.recensement.analyse_notes,
           I18n.l(objet.recensement.analysed_at, format: :long).upcase_first,
           "Manquant",
           "Œuvre déclarée manquante au moment du recensement Collectif Objets en #{objet.recensement.analysed_at.year}"
