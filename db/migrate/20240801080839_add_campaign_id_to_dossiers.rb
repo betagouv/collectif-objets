8a2dc080e3b154de1ee8927b2d323a4d17b24945
Goulven Champenois <goulven.champenois@beta.gouv.fr>
2024-08-06 10:06:47 +0200

    Ajuste le délai pour associer les dossiers aux campagnes pour correspondre aux mails de relance

--- db/migrate/20240801080839_add_campaign_id_to_dossiers.rb
+++ db/migrate/20240801080839_add_campaign_id_to_dossiers.rb
@@ -11,7 +11,7 @@ class AddCampaignIdToDossiers < ActiveRecord::Migration[7.1]
     say_with_time "Add campaign_id to dossiers" do
       Campaign.where(status: [:ongoing, :finished]).includes(:recipients).find_each do |campaign|
         commune_id = campaign.recipients.collect(&:commune_id)
-        created_at = campaign.date_lancement..(campaign.date_fin + 6.months)
+        created_at = campaign.date_lancement..(campaign.date_fin + 1.year)
         Dossier.where(campaign_id: nil, commune_id:, created_at:).update_all(campaign_id: campaign.id)
       end
       # Return the number of dossiers updated
