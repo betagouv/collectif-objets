1cd0a947d31109b67fb1b3355fc3700e22951fc4
Goulven Champenois <goulven.champenois@beta.gouv.fr>
2024-08-09 12:55:09 +0200

    Simplifie la génération du badge de statut de commune

--- app/helpers/commune_helper.rb
+++ app/helpers/commune_helper.rb
@@ -1,12 +1,11 @@
 # frozen_string_literal: true
 
-# rubocop:disable Rails/OutputSafety
 module CommuneHelper
   def commune_status_badge(commune)
     color = { inactive: "", started: :info, completed: :success }
       .with_indifferent_access[commune.status]
     text = I18n.t("activerecord.attributes.commune.statuses.#{commune.status}")
-    "<p class=\"fr-badge fr-badge--sm fr-badge--#{color}\">#{text}</p>".html_safe
+    content_tag("p", text, class: "fr-badge fr-badge--sm fr-badge--#{color}")
   end
 
   def commune_messagerie_title(commune)
@@ -44,4 +43,3 @@ module CommuneHelper
     )
   end
 end
-# rubocop:enable Rails/OutputSafety
