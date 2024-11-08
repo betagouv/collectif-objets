9c780c9eea1b14974fb1dbf87d6ac90eb03bbb17
Goulven Champenois <goulven.champenois@beta.gouv.fr>
2024-11-08 10:51:22 +0100

    Simplifie le contrôleur accordion-open

--- app/frontend/stimulus_controllers/accordion_open_controller.js
+++ app/frontend/stimulus_controllers/accordion_open_controller.js
@@ -1,15 +1,13 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["group"]
-
   initialize() {
     const anchor = window.location.hash?.replace("#", "")
     if (!anchor) return
 
     // TODO: replace with proper wait on DSFR JS init
     setTimeout(() => {
-      this.groupTarget.querySelector(`button.fr-accordion__btn[aria-controls="${anchor}"]`)?.dispatchEvent(new Event('click'));
+      this.element.querySelector(`button.fr-accordion__btn[aria-controls="${anchor}"]`)?.dispatchEvent(new Event('click'));
     }, 500)
   }
 }
