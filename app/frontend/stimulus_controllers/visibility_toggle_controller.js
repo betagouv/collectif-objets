3923ff3cc1b279bb90a5f0270fb9dcf38be48aff
Goulven Champenois <goulven.champenois@beta.gouv.fr>
2024-11-28 17:43:08 +0100

    Supprime les targets inutiles

--- app/frontend/stimulus_controllers/visibility_toggle_controller.js
+++ app/frontend/stimulus_controllers/visibility_toggle_controller.js
@@ -1,9 +1,7 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["section", "button"]
-
   toggle() {
-    this.sectionTarget.style.display = "none";
+    this.element.style.display = "none";
   }
 }
