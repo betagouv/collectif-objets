dcd2c58a11816b776bbd92429c659ee04d62eaf2
Goulven Champenois <goulven.champenois@beta.gouv.fr>
2024-11-28 18:28:27 +0100

    Améliore le contrôleur des messages
    - Utilise le target element implicite
    - Évite une fuite mémoire quand on sort de la page

--- app/frontend/stimulus_controllers/messages_controller.js
+++ app/frontend/stimulus_controllers/messages_controller.js
@@ -1,19 +1,22 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["list"]
-
   connect() {
     this.scrollToBottom()
-    new MutationObserver(
+    this.observer = new MutationObserver(
       (mutationList, _observer) => {
         if (!mutationList.some(mutation => mutation.addedNodes)) return
         this.scrollToBottom()
       }
-    ).observe(this.listTarget, { childList: true })
-  }
-  scrollToBottom() {
-    this.listTarget.scrollTop = this.listTarget.scrollHeight
+    ).observe(this.element, { childList: true })
   }
 
+  disconnect() {
+    this.observer?.disconnect()
+    this.observer = null
+  }
+
+  scrollToBottom() {
+    this.element.scrollTop = this.element.scrollHeight
+  }
 }
