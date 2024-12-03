0848fad86d5b189bd49001761e3b3bf4fb5e18d3
Goulven Champenois <goulven.champenois@beta.gouv.fr>
2024-12-03 14:01:39 +0100

    Bloque l'upload de fichiers trop lourds ou du mauvais type, en utilisant les classes d'erreurs du DSFR

--- app/components/photo_upload_component/photo_upload_component_controller.js
+++ app/components/photo_upload_component/photo_upload_component_controller.js
@@ -4,8 +4,8 @@ import Nanobar from "nanobar"
 import "./photo_upload_component.css"
 
 export default class extends Controller {
-  static targets = ["input", "submit", "nanobar", "progressText"]
-  static values = { url: String }
+  static targets = ["input", "submit", "nanobar", "display", "group"]
+  static values = { url: String, accept: String, maxFileSize: Number }
 
   connect() {
     this.submitTarget.remove()
@@ -21,16 +21,9 @@ export default class extends Controller {
     if (files.length !== 1) return
 
     this.nanobar = new Nanobar({ target: this.nanobarTarget })
-    this.progressTextTarget.innerHTML = `Chargement démarré …`
+    this.display(`Chargement démarré …`)
     this.toggleDependentLinks(false)
     this.uploadFile(files[0])
-    document.querySelector(".fr-input-group--error")?.classList.remove("fr-input-group--error")
-    document.querySelectorAll(".fr-error-text,.fr-alert.fr-alert--error").forEach(e => e.remove())
-  }
-
-  directUploadWillStoreFileWithXHR(xhr) {
-    xhr.upload.addEventListener("progress", event => this.uploadRequestDidProgress(event))
-    this.progressTextTarget.innerHTML = `Chargement en cours …`
   }
 
   toggleDependentLinks(enabled) {
@@ -39,22 +32,33 @@ export default class extends Controller {
       .forEach(link => link.toggleAttribute("disabled", !enabled))
   }
 
+  directUploadWillStoreFileWithXHR(xhr) {
+    xhr.upload.addEventListener("progress", event => this.uploadRequestDidProgress(event))
+    this.display(`Chargement en cours …`)
+  }
+
   uploadRequestDidProgress(event) {
     const progress = event.loaded / event.total * 100
     if (!progress) return
 
-    this.progressTextTarget.innerHTML = `Chargée à ${Math.round(progress)}%`
+    this.display(`Chargée à ${Math.round(progress)}%`)
     this.nanobar.go(progress)
   }
 
   uploadFile(file) {
+    // Bail out for invalid file extensions
+    if (this.invalid(file)) {
+      this.inputTarget.disabled = false
+      this.toggleDependentLinks(true)
+      return
+    }
     this.inputTarget.disabled = true // so it does re-upload on form submit
     new DirectUpload(file, this.urlValue, this).create(
       (error, blob) => {
         if (error) {
           this.inputTarget.disabled = false
           this.toggleDependentLinks(true)
-          this.progressTextTarget.innerHTML = "Échec d'envoi du fichier"
+          this.display("Échec d'envoi du fichier", { error: true })
           console.log("ERROR on direct upload !", error)
         } else {
           const hiddenField = document.createElement("input")
@@ -63,10 +67,40 @@ export default class extends Controller {
           hiddenField.name = this.inputTarget.name
           this.inputTarget.after(hiddenField)
           this.inputTarget.removeAttribute("data-uploading")
-          this.progressTextTarget.innerHTML = "Photo en cours d’ajout …"
+          this.display("Photo en cours d’ajout …")
           this.element.requestSubmit()
         }
       }
     )
   }
+
+  invalid(file) {
+    const extension = file.name.toLowerCase().split('.').pop()
+    if (!this.acceptValue.split("|").includes(extension)) {
+      this.display("Ce type de fichier n'est pas autorisé. Merci de sélectionner une photo au format jpg ou png.", { error: true })
+      return true
+    }
+    if (this.maxFileSizeValue > 0 && file.size > this.maxFileSizeValue) {
+      this.display(`Le fichier ${file.name} est trop lourd (${this.humanFileSize(file.size)}). Le poids maximum autorisé est : ${this.humanFileSize(this.maxFileSizeValue)}.`, { error: true })
+      return true
+    }
+    return false
+  }
+
+  display(message, { error = false } = {}) {
+    this.groupTarget.classList.toggle("fr-input-group--error", error)
+    this.displayTarget.classList.toggle("fr-error-text", error)
+    this.displayTarget.innerHTML = message || ""
+  }
+
+  humanFileSize(number) {
+    if (number < 1024) {
+      return `${number} octets`
+    } else if (number >= 1024 && number < 1048576) {
+      return `${(number / 1024).toFixed(0)} Ko`.replace(".", ",")
+    } else if (number >= 1048576) {
+      return `${(number / 1048576).toFixed(0)} Mo`
+    }
+  }
 }
+
