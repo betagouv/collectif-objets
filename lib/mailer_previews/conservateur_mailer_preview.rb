a895800442c235a3c220139a396ee3d41da6fcec
Goulven Champenois <goulven.champenois@beta.gouv.fr>
2024-12-17 10:03:19 +0100

    Corrige la prévisualisation du mail conservateur "nouveau message d'une commune"

--- lib/mailer_previews/conservateur_mailer_preview.rb
+++ lib/mailer_previews/conservateur_mailer_preview.rb
@@ -3,7 +3,7 @@
 class ConservateurMailerPreview < ApplicationMailerPreview
   def message_received_email
     message = Message.from_commune.first
-    conservateur = message.author
+    conservateur = Message.from_conservateur.first.author
 
     ConservateurMailer.with(message:, conservateur:).message_received_email
   end
