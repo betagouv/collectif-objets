d483d3df379531138ca785e4268749aec695af70
Goulven Champenois <goulven.champenois@userland.fr>
2025-01-07 17:07:31 +0100

    Refactorise l'authentification par code de session

--- app/models/concerns/session_code_authenticatable.rb
+++ app/models/concerns/session_code_authenticatable.rb
@@ -4,7 +4,13 @@ module SessionCodeAuthenticatable
   extend ActiveSupport::Concern
 
   included do
-    has_one :session_code, -> { valid.order(created_at: :desc) }, dependent: :destroy, as: :record
-    has_many :session_codes, dependent: :destroy # To destroy expired codes
+    has_one :session_code, -> { valid.order(created_at: :desc) }, dependent: :destroy, as: :record, inverse_of: :record
+    has_many :session_codes, as: :record, dependent: :destroy # To destroy expired codes
+  end
+
+  class_methods do
+    def authenticate_by(email:, code:)
+      SessionAuthentication.authenticate_by(email:, code:, model: self)
+    end
   end
 end
--- app/models/session_authentication.rb
+++ app/models/session_authentication.rb
@@ -8,48 +8,58 @@ class SessionAuthentication
   # the order of these validations is important
   validates :email, presence: true
   validates :code, presence: true
-  validates :user, presence: { message: "Aucun utilisateur trouvé pour cet email" }
-  validate :validate_code_format, :validate_codes_match, :validate_code_not_expired
+  validate  :validate_resource_found, :validate_code_format, :validate_codes_match, :validate_code_not_expired
 
-  def initialize(email, code)
+  delegate :session_code, to: :authenticatable
+
+  class << self
+    def authenticate_by(email:, code:, model:)
+      new(email:, code:, model:).authenticate
+    end
+  end
+
+  def initialize(email:, code:, model:)
     @email = email.to_s.strip
     @code = code.gsub(/\D+/, "")
+    @model = model
   end
 
-  def authenticate(&sign_in_block)
+  def authenticate
     prevent_timing_attacks
-    return false unless valid?
-
-    return false unless sign_in_block.call(user)
-
-    session_code.mark_used!
-    true
+    if valid?
+      session_code.mark_used!
+      authenticatable
+    else
+      [nil, errors.full_messages.to_sentence]
+    end
   end
 
-  def user
-    @user ||= User.find_by(email:)
+  def authenticatable
+    @authenticatable ||= @model.find_by(email:)
   end
-
-  def error_message = errors.map(&:message).to_sentence
 
   private
 
-  def prevent_timing_attacks
-    sleep(rand(0.5..1)) if Rails.env.production?
-  end
-
   attr_reader :email, :code
 
-  delegate :session_code, to: :user, allow_nil: true
+  def validate_resource_found
+    return if authenticatable.persisted?
+
+    errors.add(
+      :email,
+      :not_found,
+      message: "Aucun compte trouvé pour cet email"
+    )
+  end
 
   def validate_code_format
     return if errors.any? || SessionCode.valid_format?(code)
 
     errors.add(
       :code,
-      :invalid_format,
+      :invalid,
       message: "Le code de connexion est composé de #{SessionCode::LENGTH} chiffres exactement. " \
-               "Vérifiez que vous n’avez pas copié deux fois le même chiffre."
+               "Vérifiez que vous n'avez pas copié deux fois le même chiffre."
     )
   end
 
@@ -62,7 +72,7 @@ class SessionAuthentication
       :code,
       :mismatch,
       message: "Code de connexion incorrect. " \
-               "Vérifiez que vous avez bien recopié le code et qu’il provient bien du dernier mail envoyé."
+               "Vérifiez que vous avez bien recopié le code et qu'il provient bien du dernier mail envoyé."
     )
   end
 
@@ -71,7 +81,14 @@ class SessionAuthentication
 
     return unless session_code.expired?
 
-    errors.add :code, :expired, message: "Le code de connexion n'est plus valide, " \
-                                         "veuillez en redemander un en cliquant sur le bouton ci-dessous"
+    errors.add(
+      :code,
+      :expired,
+      message: "Le code de connexion n'est plus valide, veuillez en demander un nouveau."
+    )
+  end
+
+  def prevent_timing_attacks
+    sleep(rand(0.5..1)) if Rails.env.production?
   end
 end
