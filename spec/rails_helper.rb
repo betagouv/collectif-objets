ed59715582e82f75212741be835552d1b5973c6d
Goulven Champenois <goulven.champenois@userland.fr>
2025-04-23 15:58:09 +0200

    Ajoute un helper `dom_id` pour les feature specs

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -21,6 +21,12 @@ rescue ActiveRecord::PendingMigrationError => e
   exit 1
 end
 
+module CapybaraDomId
+  def dom_id(element)
+    "#" + ActionView::RecordIdentifier.dom_id(element)
+  end
+end
+
 RSpec.configure do |config|
   config.fixture_paths = [Rails.root.join("/spec/fixtures")]
   config.use_transactional_fixtures = true
@@ -30,6 +36,7 @@ RSpec.configure do |config|
 
   config.include Warden::Test::Helpers
   config.before(:suite) { require Rails.root.join("scripts/create_postgres_sequences_memoire_photos_numbers.rb") }
+  config.include CapybaraDomId, type: :feature
 
   require "webmock/rspec"
   WebMock.disable_net_connect!(allow_localhost: true)
