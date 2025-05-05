4439af79e27149f7f0840eb5aa35007e7d6d2393
Goulven Champenois <goulven.champenois@userland.fr>
2025-05-05 16:16:59 +0200

    Précise la méthode testée dans edifice_spec

--- spec/models/edifice_spec.rb
+++ spec/models/edifice_spec.rb
@@ -3,17 +3,18 @@
 require "rails_helper"
 
 RSpec.describe Edifice, type: :model do
-  it "filtre les edifices avec des objets classés ou inscrits" do
-    edifice_sans_objets_classés_ou_inscrits = create(:edifice)
-    edifice_avec_objets_classés_ou_inscrits = create(:edifice)
+  describe ".with_objets_classés_ou_inscrits" do
+    it "filtre les edifices avec des objets classés ou inscrits" do
+      edifice_sans_objets_classés_ou_inscrits = create(:edifice)
+      edifice_avec_objets_classés_ou_inscrits = create(:edifice)
 
-    create(:objet, palissy_PROT: "inscrit au titre objet", edifice: edifice_avec_objets_classés_ou_inscrits)
-    create(:objet, palissy_PROT: "classé au titre objet", edifice: edifice_avec_objets_classés_ou_inscrits)
-    create(:objet,
-           palissy_PROT: "classé au titre objet ; classé au titre objet",
-           edifice: edifice_avec_objets_classés_ou_inscrits)
-    create(:objet, palissy_PROT: "désinscrit", edifice: edifice_sans_objets_classés_ou_inscrits)
+      create(:objet, palissy_PROT: "inscrit au titre objet", edifice: edifice_avec_objets_classés_ou_inscrits)
+      create(:objet, palissy_PROT: "classé au titre objet", edifice: edifice_avec_objets_classés_ou_inscrits)
+      create(:objet, palissy_PROT: "classé au titre objet ; classé au titre objet",
+                     edifice: edifice_avec_objets_classés_ou_inscrits)
+      create(:objet, palissy_PROT: "désinscrit", edifice: edifice_sans_objets_classés_ou_inscrits)
 
-    expect(Edifice.with_objets_classés_ou_inscrits.count).to eq({ edifice_avec_objets_classés_ou_inscrits.id => 3 })
+      expect(Edifice.with_objets_classés_ou_inscrits.count).to eq({ edifice_avec_objets_classés_ou_inscrits.id => 3 })
+    end
   end
 end
