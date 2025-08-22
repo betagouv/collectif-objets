182c6e24183eda6230833d79e688585ca8ca5687
Goulven Champenois <goulven.champenois@userland.fr>
2025-08-22 16:48:02 +0200

    fix: Ajoute les tables recenseurs et recenseur_accesses

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_04_23_085432) do
+ActiveRecord::Schema[7.2].define(version: 2025_07_15_162402) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "unaccent"
@@ -432,6 +432,31 @@ ActiveRecord::Schema[7.2].define(version: 2025_04_23_085432) do
     t.index ["objet_id", "dossier_id"], name: "index_recensements_on_objet_id_and_dossier_id", unique: true
   end
 
+  create_table "recenseur_accesses", force: :cascade do |t|
+    t.bigint "recenseur_id", null: false
+    t.bigint "commune_id", null: false
+    t.boolean "granted"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.boolean "notified", default: false, null: false
+    t.index ["commune_id"], name: "index_recenseur_accesses_on_commune_id"
+    t.index ["recenseur_id", "commune_id"], name: "index_recenseur_accesses_on_recenseur_id_and_commune_id", unique: true
+    t.index ["recenseur_id"], name: "index_recenseur_accesses_on_recenseur_id"
+  end
+
+  create_table "recenseurs", force: :cascade do |t|
+    t.string "email", null: false
+    t.string "status", default: "pending", null: false
+    t.string "nom"
+    t.text "notes"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.datetime "remember_created_at"
+    t.datetime "last_sign_in_at"
+    t.boolean "premiere_visite", default: true, null: false
+    t.index ["email"], name: "index_recenseurs_on_email", unique: true
+  end
+
   create_table "session_codes", force: :cascade do |t|
     t.bigint "record_id", null: false
     t.string "code"
@@ -480,5 +505,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_04_23_085432) do
   add_foreign_key "dossiers", "communes"
   add_foreign_key "messages", "communes"
   add_foreign_key "recensements", "objets"
+  add_foreign_key "recenseur_accesses", "communes"
+  add_foreign_key "recenseur_accesses", "recenseurs"
   add_foreign_key "users", "communes"
 end
