c81b792b5c53c696514b89fc7abad20ec5a82016
Goulven Champenois <goulven.champenois@userland.fr>
2025-10-02 10:42:51 +0200

    config: Fetch Sentry DSN from ENV or credentials

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,7 +1,7 @@
 Sentry.init do |config|
   return unless Rails.env.production?
 
-  config.dsn = 'https://5f6f9cf638ac413b82d1d9c8a9ba2025@sentry.incubateur.net/40'
+  config.dsn = ENV.fetch("SENTRY_DSN", Rails.application.credentials.sentry.dsn)
   config.breadcrumbs_logger = [:active_support_logger, :http_logger]
 
   # Set tracesSampleRate to 1.0 to capture 100%
