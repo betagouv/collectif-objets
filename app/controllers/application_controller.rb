acf296b459b2b0cba5aded3b90b1d9c7bb5d626d
Goulven Champenois <goulven.champenois@userland.fr>
2025-10-03 13:10:03 +0200

    refactor: Simplify and centralise checking for staging environment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -97,7 +97,7 @@ class ApplicationController < ActionController::Base
 
   def init_banners
     @banners = []
-    @banners << :environment if %w[development staging].include?(Rails.configuration.x.environment_specific_name)
+    @banners << :environment if Rails.env.development? || Rails.application.staging?
     @banners << :user_impersonate if impersonating_user?
     @banners << :conservateur_impersonate if current_conservateur.present? && current_conservateur != true_conservateur
   end
