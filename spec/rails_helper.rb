db5e6749dbcd0f1d4569eb272006e14183b6850b
Goulven Champenois <goulven.champenois@beta.gouv.fr>
2025-12-22 11:09:34 +0100

    Fix: flaky specs (#1509)
    * Add `dom_id` helper for feature specs
    * Fix flaky features using DatabaseCleaner instead of transactions
    * Clear Warden sessions after each feature spec
    * Sleep after feature specs to prevent truncating ActiveStorage while images requests are still pending
    * Extract feature spec setup into a dedicated file for readability

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -10,8 +10,9 @@ require "rspec/rails"
 
 # Add additional requires below this line. Rails is not loaded until this point!
 require "axe-rspec"
+require "database_cleaner/active_record"
 
-Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
+Rails.root.glob("spec/support/**/*.rb").each { |f| require f }
 
 # Checks for pending migrations and applies them before tests are run.
 begin
@@ -22,56 +23,20 @@ rescue ActiveRecord::PendingMigrationError => e
 end
 
 RSpec.configure do |config|
-  config.fixture_paths = [Rails.root.join("/spec/fixtures")]
-  config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
   # config.filter_gems_from_backtrace("gem name")
 
+  config.fixture_paths = [Rails.root.join("/spec/fixtures")]
+
+  config.use_transactional_fixtures = true
+
   config.include Warden::Test::Helpers
   config.include Devise::Test::ControllerHelpers, type: :controller
+
+  # Add app-specific columns to ActiveStorage
   config.before(:suite) { require Rails.root.join("scripts/create_postgres_sequences_memoire_photos_numbers.rb") }
-
-  require "webmock/rspec"
-  WebMock.disable_net_connect!(allow_localhost: true)
-  config.before(type: :feature) do
-    stub_request(:any, /tube.numerique.gouv.fr/).to_return(status: 200, body: "", headers: {})
-    # Silence upstream deprecation warning. See https://github.com/teamcapybara/capybara/issues/2779
-    Selenium::WebDriver.logger.ignore(:clear_local_storage, :clear_session_storage)
-  end
-
-  config.after(type: :feature) do |example_group|
-    # Clear sessions and Warden state after each feature spec to prevent leakage
-    Capybara.reset_sessions!
-    Warden.test_reset!
-
-    # Save screenshot only on failure
-    next unless example_group.exception
-
-    r = save_screenshot
-    puts "saved screenshot to #{r}"
-  end
 end
 
-Capybara.register_driver :headless_firefox do |app|
-  options = Selenium::WebDriver::Firefox::Options.new
-  options.add_argument "-headless"
-  Capybara::Selenium::Driver.new app, browser: :firefox, options:
-end
-
-Capybara.register_driver :firefox do |app|
-  options = Selenium::WebDriver::Firefox::Options.new
-  Capybara::Selenium::Driver.new app, browser: :firefox, options:
-end
-
-Capybara.register_driver :chrome do |app|
-  options = Selenium::WebDriver::Chrome::Options.new
-  Capybara::Selenium::Driver.new app, browser: :chrome, options:
-end
-
-Capybara.server = :puma, { Silent: true }
-Capybara.javascript_driver = ENV.fetch("CAPYBARA_JS_DRIVER", "headless_firefox").to_sym
-Capybara.save_path = Rails.root.join("tmp/artifacts/capybara")
-
-Capybara.default_max_wait_time = 10
-Capybara.server_port = 31337
+require "webmock/rspec"
+WebMock.disable_net_connect!(allow_localhost: true)
