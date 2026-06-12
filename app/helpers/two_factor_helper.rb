28d7dd1e3cf867ace2de0607723f5be10ab8ea5a
Goulven Champenois <goulven.champenois@userland.fr>
2026-06-12 17:09:31 +0200

    Add a class to the generated qrcode and refactor for readability

--- app/helpers/two_factor_helper.rb
+++ app/helpers/two_factor_helper.rb
@@ -1,18 +1,23 @@
 # frozen_string_literal: true
 
 module TwoFactorHelper
-  def otp_qrcode(user)
+  def otp_qrcode(user, **options)
     require "rqrcode"
 
+    issuer = otp_issuer
+    label = "#{issuer}:#{user.email}"
+    uri = user.otp_provisioning_uri(label, issuer:)
+    qr = RQRCode::QRCode.new(uri)
+    qr.as_svg(module_size: 4, color: "000", fill: "fff", svg_attributes: options)
+      .html_safe # rubocop:disable Rails/OutputSafety
+  end
+
+  def otp_issuer
     environment = if Rails.application.staging?
                     "(staging)"
                   elsif !Rails.env.production?
                     Rails.env
                   end
-    issuer = ["Collectif Objets", environment].compact.join(" ")
-    label = "#{issuer}:#{user.email}"
-    uri = user.otp_provisioning_uri(label, issuer:)
-    qr = RQRCode::QRCode.new(uri)
-    qr.as_svg(module_size: 4, color: "000", fill: "fff").html_safe # rubocop:disable Rails/OutputSafety
+    ["Collectif Objets", environment].compact.join(" ")
   end
 end
